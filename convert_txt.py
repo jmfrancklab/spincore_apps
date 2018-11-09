28b5e1826d12e32a536389625b0fdcdb85c64257
Alec Beaton (SU Network) <aabeaton@syr.edu>
2018-11-09 18:46:13 -0500

    Simplifying code

--- convert_txt.py
+++ convert_txt.py
@@ -8,21 +8,17 @@ file_name = "Hahn_echo"
 F = open(directory+"\\"+file_name+".txt", "r")
 real = []
 imag = []
+result = []
 for line in F:
     temp = line.strip().split()
-    real.append(float(temp[0]))
-    imag.append(float(temp[1]))
-    #real = temp[0]
-    #imag = temp[1]
-    #result.append(temp[0]+1j*temp[1])
+    result.append(complex128(float(temp[0]))+1j*complex128(float(temp[1])))
+    #real.append(float(temp[0]))
+    #imag.append(float(temp[1]))
 F.close()
-print real[0]
-print imag[0]
-print size(real)
-print size(imag)
-real = complex128(real)
-imag = complex128(imag)
-result = complex128(real + 1j*imag) 
+quit()
+#result = complex128(real) + 1j*complex128(imag) 
+if (complex128(real[0]) + 1j*complex128(imag[0]) != result[0]):
+        raise ValueError('Quadrature data incorrectly cast to complex array')
 print result[0]
 print real[0]
 print imag[0]
@@ -30,13 +26,10 @@ print shape(result)
 SW = 60e3 # get this to be read in, i.e. print in the
             # text file output from SpinCore program
 acq_time = float(size(real))/SW
-print acq_time
+print "ACQUISITION TIME:",acq_time
 dt = acq_time/float(size(real))
-print dt
+print "DWELL TIME:",dt
 time_axis = linspace(0.0,acq_time,float(size(real)))
-print time_axis
-print shape(time_axis)
-print shape(result)
 data = nddata(result,'t')
 data.setaxis('t',time_axis)
 print data
