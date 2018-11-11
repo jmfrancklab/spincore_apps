3b23a5ebb6dbd5f3a04fdabaf3ae76ffc3a07d35
aabeaton <aabeaton@syr.edu>
2018-11-11 13:57:49 -0500

    Updated plotting to pull relevant parameters from the _params.txt file generated during data acquisition

--- proc_quick.py
+++ proc_quick.py
@@ -14,24 +14,33 @@ for date,id_string in [
         #('181110','noise_3'), # SW = 200 kHz
         #('181110','noise_4'), # SW = 200 kHz
         #('181110','noise_5'), # SW = 200 kHz
-        ('181110','SE_1'), # SW = 200 kHz
-        ('181110','SE_2'), # SW = 200 kHz
-        ('181110','SE_3'), # SW = 200 kHz
-        ('181110','SE_4'), # SW = 200 kHz
+        #('181110','SE_1'), # SW = 200 kHz
+        #('181110','SE_2'), # SW = 200 kHz
+        #('181110','SE_3'), # SW = 200 kHz
+        #('181110','SE_4'), # SW = 200 kHz
         ('181110','SE_5'), # SW = 200 kHz
         ('181110','SE_6'), # SW = 200 kHz
-        ('181110','SE_7'), # SW = 200 kHz
+        #('181110','SE_7'), # SW = 200 kHz
         ]:
     filename = date+'_'+id_string+'.h5'
     nodename = 'signal'
-    label=id_string
+    with open(getDATADIR(exp_type='test_equip')+date+'_'+id_string+'_params.txt', "r") as F:
+        for line in F:
+            if 'tauDelay_us=' in line:
+                temp = line.strip().split()
+                temp = temp[1].split('=')
+                tauDelay_us = float(temp[1])
+    label=r'$\tau$=%0.1f ms'%(tauDelay_us/1e3)
     s = nddata_hdf5(filename+'/'+nodename,
             directory = getDATADIR(
                 exp_type = 'test_equip'))
     s.set_units('t','s')
-    fl.next('capture, t')
-    fl.plot(s,alpha=0.4,label='%s'%label)
-    s.ft('t',shift=True)
-    fl.next('capture, f')
-    fl.plot(s,alpha=0.4,label='%s'%label)
+    fl.next('Capture,%s'%id_string)
+    fl.plot(s.real['t':(0,20e-3)],alpha=0.4,label='real, %s'%label)
+    fl.plot(s.imag['t':(0,20e-3)],alpha=0.4,label='imag, %s'%label)
+    fl.next('All captures, t')
+    fl.plot(s['t':(0,20e-3)],alpha=0.4,label='%s'%label)
+    #s.ft('t',shift=True)
+    #fl.next('capture, f')
+    #fl.plot(s,alpha=0.4,label='%s'%label)
 fl.show()
