07e38c9c56340b8dd7297caed86f022fa5fb4a8c
Alec Beaton (SU Network) <aabeaton@syr.edu>
2018-11-11 21:45:19 -0500

    Updated SpinCore processing plots

--- proc_quick.py
+++ proc_quick.py
@@ -14,12 +14,12 @@ for date,id_string in [
         #('181110','noise_3'), # SW = 200 kHz
         #('181110','noise_4'), # SW = 200 kHz
         #('181110','noise_5'), # SW = 200 kHz
-        #('181110','SE_1'), # SW = 200 kHz
+        ('181110','SE_1'), # SW = 200 kHz
         #('181110','SE_2'), # SW = 200 kHz
         #('181110','SE_3'), # SW = 200 kHz
         #('181110','SE_4'), # SW = 200 kHz
-        ('181110','SE_5'), # SW = 200 kHz
-        ('181110','SE_6'), # SW = 200 kHz
+        #('181110','SE_5'), # SW = 200 kHz
+        #('181110','SE_6'), # SW = 200 kHz
         #('181110','SE_7'), # SW = 200 kHz
         ]:
     filename = date+'_'+id_string+'.h5'
@@ -30,14 +30,23 @@ for date,id_string in [
                 temp = line.strip().split()
                 temp = temp[1].split('=')
                 tauDelay_us = float(temp[1])
+            if 'nScans=' in line:
+                temp = line.strip().split()
+                temp = temp[1].split('=')
+                nScans = int(temp[1])
     label=r'$\tau$=%0.1f ms'%(tauDelay_us/1e3)
     s = nddata_hdf5(filename+'/'+nodename,
             directory = getDATADIR(
                 exp_type = 'test_equip'))
     s.set_units('t','s')
-    fl.next('Capture,%s'%id_string)
-    fl.plot(s.real['t':(0,20e-3)],alpha=0.4,label='real, %s'%label)
-    fl.plot(s.imag['t':(0,20e-3)],alpha=0.4,label='imag, %s'%label)
+    fl.next('SpinCore Spin Echo, %d scan'%nScans)
+    fl.plot(s.real,alpha=0.4,c='r',label='real')
+    fl.plot(s.imag,alpha=0.4,c='blue',label='imag')
+    fl.next('SpinCore Spin Echo, %d scan (zoom)'%nScans)
+    fl.plot(s.real['t':(0,1.3e-3)],alpha=0.4,c='r',label='real')
+    fl.plot(s.imag['t':(0,1.3e-3)],alpha=0.4,c='blue',label='imag')
+    axvline(60,c='k',linestyle=':')
+    fl.show();quit()
     fl.next('All captures, t')
     fl.plot(s['t':(0,20e-3)],alpha=0.4,label='%s'%label)
     #s.ft('t',shift=True)
