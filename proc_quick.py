c99641b27c89c72e4087fff7e322e15808526afc
Alec Beaton (SU Network) <aabeaton@syr.edu>
2018-11-14 23:20:43 -0500

    Illustrating effect of filtering (on nutation curve data, yet to push)

--- proc_quick.py
+++ proc_quick.py
@@ -1,27 +1,7 @@
 from pyspecdata import *
 fl = figlist_var()
 for date,id_string in [
-        #('181109','SE_2'), # SW = 10 kHz
-        #('181109','test_2') # SW = 10 kHz
-        #('181109','test_3'), # SW = 1 kHz
-        #('181109','SE_3') # SW = 1 kHz
-        #('181109','SE_4'), # SW = 200 kHz
-        #('181109','test_4') # SW = 200 kHz
-        #('181109','SE_5') # SW = 200 kHz
-        #('181110','SE_1'), # SW = 200 kHz
-        #('181110','noise_1'), # SW = 200 kHz
-        #('181110','noise_2'), # SW = 200 kHz
-        #('181110','noise_3'), # SW = 200 kHz
-        #('181110','noise_4'), # SW = 200 kHz
-        #('181110','noise_5'), # SW = 200 kHz
-        #('181110','SE_1'), # SW = 200 kHz
-        #('181110','SE_2'), # SW = 200 kHz
-        #('181110','SE_3'), # SW = 200 kHz
-        #('181110','SE_4'), # SW = 200 kHz
-        ('181110','SE_5'), # SW = 200 kHz
-        ('181110','SE_6'), # SW = 200 kHz
-        ('181110','SE_7'), # SW = 200 kHz
-        ('181110','SE_8'), # SW = 200 kHz
+        ('181114','nutation_1'),
         ]:
     filename = date+'_'+id_string+'.h5'
     nodename = 'signal'
@@ -40,6 +20,16 @@ for date,id_string in [
             directory = getDATADIR(
                 exp_type = 'test_equip'))
     s.set_units('t','s')
+    fl.next('Plotting nutation')
+    fl.plot(abs(s))
+    s.ft('t',shift=True)
+    fl.next('F plot')
+    fl.plot(s)
+    s = s['t':(-10e3,10e3)]
+    s.ift('t')
+    fl.next('Filtered')
+    fl.plot(abs(s))
+    fl.show();quit()
     #fl.next('SpinCore Spin Echo, %d scan(s)'%nScans)
     #fl.plot(s.real,alpha=0.4,c='r',label='real')
     #fl.plot(s.imag,alpha=0.4,c='blue',label='imag')
@@ -47,7 +37,7 @@ for date,id_string in [
     #fl.plot(s.real['t':(0,7e-3)],alpha=0.4,c='r',label='real')
     #fl.plot(s.imag['t':(0,7e-3)],alpha=0.4,c='blue',label='imag')
     #axvline(4.9,c='k',linestyle=':')
-    fl.next('SpinCore Spin Echoes, 20 scans')
+    fl.next('SpinCore CPMG, 16 echoes')
     fl.plot(s['t':(0,40e-3)],alpha=0.4,label='%s'%label)
     #s.ft('t',shift=True)
     #fl.next('capture, f')
