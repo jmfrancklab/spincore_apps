bca418258d26d470448bb2e5c88962a311b8c012
Alec Beaton (SU Network) <aabeaton@syr.edu>
2018-11-25 17:04:05 -0500

    Working on processing CPMG data

--- proc_quick.py
+++ proc_quick.py
@@ -1,17 +1,8 @@
 from pyspecdata import *
 fl = figlist_var()
 for date,id_string in [
-        ('181121','nutation_10'),
-        #('181121','nutation_8'),
-        #('181121','nutation_7'),
-        #('181121','nutation_6'),
-        #('181121','nutation_5'),
-        #('181121','nutation_4'),
-        #('181121','nutation_2'),
-        #('181120','nutation_2'),
-        #('181120','nutation90_1'),
-        #('181114','nutation_5'),
-        #('181113','CPMG_8'),
+        ('181121','CPMG_5'),
+        #('181121','nutation_10'),
         ]:
     filename = date+'_'+id_string+'.h5'
     nodename = 'signal'
@@ -21,6 +12,10 @@ for date,id_string in [
                 temp = line.strip().split()
                 temp = temp[1].split('=')
                 nPoints = float(temp[1])
+            if 'nEchoes=' in line:
+                temp = line.strip().split()
+                temp = temp[1].split('=')
+                nEchoes = float(temp[1])
             if 'nPoints_Nutation=' in line:
                 temp = line.strip().split()
                 temp = temp[1].split('=')
@@ -60,10 +55,10 @@ for date,id_string in [
         phase = True
         if phase:
             s.ft('t',shift=True)
-            ph_corr = exp(1j*2*pi*0.28)
+            ph_corr = exp(1j*2*pi*0.29)
             s *= ph_corr
             s.ift('t')
-            fl.next('phased data: nutation curve')
+            fl.next('phased data')
             fl.plot(s.real,alpha=0.5,label='real')
             fl.plot(s.imag,alpha=0.5,label='imag')
         #}}}
@@ -99,15 +94,76 @@ for date,id_string in [
         fl.show();quit()
         #}}}
     #fl.next('SpinCore Spin Echo, %d scan(s)'%nScans)
+    s.ft('t',shift=True)
+    s *= exp(1j*2*pi*0.3)
+    s.ift('t')
     fl.next('CPMG')
     fl.plot(s.real,alpha=0.4,c='r',label='real')
     fl.plot(s.imag,alpha=0.4,c='blue',label='imag')
-    fl.next('SpinCore Spin Echo, %d scan(s) (zoom)'%nScans)
-    fl.plot(s.real['t':(0,7e-3)],alpha=0.4,c='r',label='real')
-    fl.plot(s.imag['t':(0,7e-3)],alpha=0.4,c='blue',label='imag')
-    axvline(4.9,c='k',linestyle=':')
-    fl.next('CPMG, 16 echoes')
-    fl.plot(s['t':(0,40e-3)],alpha=0.4,label='%s'%label)
+    fl.next('CPMG, abs')
+    fl.plot(abs(s),alpha=0.4)
+    with open(getDATADIR(exp_type='test_equip')+date+'_'+id_string+'.txt', "r") as F:
+        for line in F:
+            temp = line.strip().split()
+            if temp[0] == 'SPECTRAL':
+                SW = float(temp[-1])
+        F.close()
+    indirect_points = nEchoes
+    indirect_acq_time = nPoints/SW
+    indirect_dt = indirect_acq_time/indirect_points
+    indirect_time_axis = linspace(0.0,indirect_acq_time,nPoints)
+    print ndshape(s)
+    CPMG_data = s.C
+    CPMG_data.setaxis('t',None)
+    CPMG_data.chunk('t',['echo','t2'],[int(indirect_points),len(indirect_time_axis)])
+    CPMG_data.setaxis('echo',r_[0:indirect_points:1])
+    CPMG_data.setaxis('t2',indirect_time_axis).set_units('t2','s')
+    print ndshape(CPMG_data)
+    fl.next('image CPMG')
+    fl.image(CPMG_data)
+    even_echo = ndshape([len(r_[0:indirect_points/2.0:1]),len(indirect_time_axis)],
+            ['echo','t2']).alloc(dtype=complex128)
+    odd_echo = ndshape([len(r_[0:indirect_points/2.0:1]),len(indirect_time_axis)],
+            ['echo','t2']).alloc(dtype=complex128)
+    even_echo.setaxis('echo',r_[0:indirect_points/2.0:1])
+    even_echo.setaxis('t2',indirect_time_axis).set_units('t2','s')
+    odd_echo.setaxis('echo',r_[0:indirect_points/2.0:1])
+    odd_echo.setaxis('t2',indirect_time_axis).set_units('t2','s')
+    print ndshape(even_echo)
+    print ndshape(odd_echo)
+    for index,echo_count in enumerate(r_[0:int(indirect_points):2]):
+        even_echo['echo',index] = CPMG_data['echo',echo_count].C
+        odd_echo['echo',index] = CPMG_data['echo',echo_count+1].C
+    fl.next('image CPMG, even echo')
+    fl.image(even_echo)
+    fl.next('image CPMG, odd echo')
+    fl.image(odd_echo)
+    even_echo.sum('t2')
+    odd_echo.sum('t2')
+    fl.next('CPMG, even echo')
+    fl.plot(even_echo,'.')
+    fl.next('CPMG, odd echo')
+    fl.plot(odd_echo,'.')
+    CPMG_data.sum('t2')
+    fl.next('CPMG, all echoes')
+    fl.plot(abs(CPMG_data),'.')
+    fl.show();quit()
+    fl.image(CPMG_data['echo',r_[0:128:2]])
+    fl.show();quit()
+    fl.next('plotting CPMG')
+    fl.plot(CPMG_data)
+    fl.show()
+    #for echo_index in r_[0:128:1]:
+    #    CPMG_data = ndshape([int(indirect_points),len(indirect_time_axis)],
+    #            ['echo','t2']).alloc(dtype=complex128)
+    #    CPMG_data.setaxis('t2',indirect_time_axis).set_units('t2','s')
+    #    CPMG_data.setaxis('echo',r_[0:indirect_points:1])
+    #CPMG_data['echo',index] = result
+    quit()
+    time_axis = s.getaxis('t')
+    acq_time = time_axis[-1]
+
+    print ndshape(s)
     #s.ft('t',shift=True)
     #fl.next('capture, f')
     #fl.plot(s,alpha=0.4,label='%s'%label)
