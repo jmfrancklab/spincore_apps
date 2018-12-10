d00cde596beac497028ab88dfa1e347aa6e137ee
Alec Beaton (SU Network) <aabeaton@syr.edu>
2018-12-10 16:26:36 -0500

    Updated shell scripts

--- swig/Hahn_echo/compile_Hahn_echo.sh
+++ swig/Hahn_echo/compile_Hahn_echo.sh
@@ -1,14 +1,32 @@
 #!/usr/bin/bash
+echo "about to compile wrapper with swig..."
 swig -python -o Hahn_echo_wrap.c Hahn_echo.i 
 # need defined in .bashrc
 # export conda_headers='C:\ProgramData\Anaconda2\include'
 # export conda_libs='C:\ProgramData\Anaconda2\libs'
+# export spincore='C:\apps-su\spincore_apps'
+# export numpy='C:\ProgramData\Anaconda2\lib\site-packages\numpy\core\include'
 if [ -z "$conda_headers" ]; then
-    echo "conda_headers isn't defined -- see comments here and define in .bashrc"
+    echo "conda_headers environment variable is not defined"
+    echo "define in .bashrc"
     exit
 fi
 if [ -z "$conda_libs" ]; then
-    echo "conda_libs isn't defined -- see comments here and define in .bashrc"
+    echo "conda_libs environment variable is not defined"
+    echo "define in .bashrc"
     exit
 fi
+if [ -z "$spincore" ]; then
+    echo "spincore environment variable is not defined"
+    echo "define in .bashrc"
+    exit
+fi
+if [ -z "$numpy" ]; then
+    echo "numpy environment variable is not defined"
+    echo "define in .bashrc"
+    exit
+fi
+echo "wrapper compiled."
+echo "about to compile module..."
 gcc -shared -DMS_WIN64 -I$conda_headers -I$spincore -I$numpy -L$conda_libs -L$spincore Hahn_echo.c Hahn_echo_wrap.c -lpython27 -lmrispinapi64 -o _Hahn_echo.pyd
+echo "module compiled."
--- swig/compile_swig.sh
+++ swig/compile_swig.sh
@@ -1,6 +1,5 @@
-# run from directory containing your scripts to compile
-# $ ./../compile_swig.sh
+#!/usr/bin/bash
 echo "about to compile..."
-winpty ./../swig.exe -python -o Hahn_echo_wrap.c Hahn_echo.i
-gcc -shared -DMS_WIN64 -I$conda_headers -I'C:\apps-su\spincore_apps' -I'C:\ProgramData\Anaconda2\lib\site-packages\numpy\core\include' -L$conda_libs -L'C:\apps-su\spincore_apps' Hahn_echo.c Hahn_echo_wrap.c -lpython27 -lmrispinapi64 -o _Hahn_echo.pyd
+swig -python -o Hahn_echo_wrap.c Hahn_echo.i
+gcc -shared -DMS_WIN64 -I$conda_headers -I$spincore -I$numpy -L$conda_libs -L$spincore Hahn_echo.c Hahn_echo_wrap.c -lpython27 -lmrispinapi64 -o _Hahn_echo.pyd
 echo "compiled."
