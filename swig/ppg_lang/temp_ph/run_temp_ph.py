0b0e2839632c6e0717aea3db85282f34fd8eac26
Alec Beaton (SU Network) <aabeaton@syr.edu>
2018-12-12 19:07:00 -0500

    Added documentation for phase cycling syntax

--- swig/ppg_lang/temp_ph/run_temp_ph.py
+++ swig/ppg_lang/temp_ph/run_temp_ph.py
@@ -1,3 +1,38 @@
+'''
+FOR PHASE CYCLING: Provide both a phase cycle label (e.g.,
+'ph1', 'ph2') as str and an array containing the indices
+(i.e., registers) of the phases you which to use that are
+specified in the numpy array 'tx_phases'.  Note that
+specifying the same phase cycle label will loop the
+corresponding phase steps together, regardless of whether
+the indices are the same or not.
+    e.g.,
+    The following:
+        ('pulse',2.0,'ph1',r_[0,1]),
+        ('delay',1.5),
+        ('pulse',2.0,'ph1',r_[2,3]),
+    will provide two transients with phases of the two pulses (p1,p2):
+        (0,2)
+        (1,3)
+    whereas the following:
+        ('pulse',2.0,'ph1',r_[0,1]),
+        ('delay',1.5),
+        ('pulse',2.0,'ph2',r_[2,3]),
+    will provide four transients with phases of the two pulses (p1,p2):
+        (0,2)
+        (0,3)
+        (1,2)
+        (1,3)
+FURTHER: The total number of transients that will be
+collected are determined by both nScans (determined when
+calling the appropriate marker) and the number of steps
+calculated in the phase cycle as shown above.  Thus for
+nScans = 1, the SpinCore will trigger 2 times in the first
+case and 4 times in the second case.  for nScans = 2, the
+SpinCore will trigger 4 times in the first case and 8 times
+in the second case.
+'''
+
 from numpy import *
 import ppg_temp_ph
 
