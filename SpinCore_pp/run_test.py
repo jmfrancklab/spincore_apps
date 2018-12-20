6d6401567d0b2e158621c10c24377791c9dd5353
Alec Beaton (SU Network) <aabeaton@syr.edu>
2018-12-20 17:33:04 -0500

    Python file for testing out SpinCore pp compatibilities

---
+++ SpinCore_pp/run_test.py
@@ -0,0 +1,135 @@
+#{{{ note on phase cycling
+'''
+FOR PHASE CYCLING: Provide both a phase cycle label (e.g.,
+'ph1', 'ph2') as str and an array containing the indices
+(i.e., registers) of the phases you which to use that are
+specified in the numpy array 'tx_phases'.  Note that
+specifying the same phase cycle label will loop the
+corresponding phase steps together, regardless of whether
+the indices are the same or not.
+    e.g.,
+    The following:
+        ('pulse',2.0,'ph1',r_[0,1]),
+        ('delay',1.5),
+        ('pulse',2.0,'ph1',r_[2,3]),
+    will provide two transients with phases of the two pulses (p1,p2):
+        (0,2)
+        (1,3)
+    whereas the following:
+        ('pulse',2.0,'ph1',r_[0,1]),
+        ('delay',1.5),
+        ('pulse',2.0,'ph2',r_[2,3]),
+    will provide four transients with phases of the two pulses (p1,p2):
+        (0,2)
+        (0,3)
+        (1,2)
+        (1,3)
+FURTHER: The total number of transients that will be
+collected are determined by both nScans (determined when
+calling the appropriate marker) and the number of steps
+calculated in the phase cycle as shown above.  Thus for
+nScans = 1, the SpinCore will trigger 2 times in the first
+case and 4 times in the second case.  for nScans = 2, the
+SpinCore will trigger 4 times in the first case and 8 times
+in the second case.
+'''
+#}}}
+from pyspecdata import *
+from numpy import *
+import SpinCore_pp 
+fl = figlist_var()
+date = '181217'
+output_name = 'CPMG_2'
+adcOffset = 46
+carrierFreq_MHz = 14.46 
+tx_phases = r_[0.0,90.0,180.0,270.0]
+amplitude = 1.0
+SW_kHz = 25.0
+nPoints = 128
+nScans = 1
+#{{{ # bit of a hack here to accomplish what JF suggested
+     # where I set nEchoes = nScans here
+     # which is to acquire several scans in separate 'buffers'
+     # or segments, and nEchoes is used
+     # to set the number of segments...
+nEchoes = 4 
+#}}}
+nPhaseSteps =  8 # should come up with way to determine this offhand
+                # or rather, from the phase programs that we want to send
+                # to the SpinCore -- FOR USE IN SETTING UP RECEIVER ONLY
+# NOTE: Number of segments is nEchoes * nPhaseSteps
+p90 = 5.0 
+tau = 2800.0
+transient = 500.0
+repetition = 2e6 # us
+print "\n*** *** ***\n"
+print "CONFIGURING TRANSMITTER..."
+SpinCore_pp.configureTX(adcOffset, carrierFreq_MHz, tx_phases, amplitude, nPoints)
+print "\nTRANSMITTER CONFIGURED."
+print "***"
+print "CONFIGURING RECEIVER..."
+acq_time = SpinCore_pp.configureRX(SW_kHz, nPoints, nScans, nEchoes, nPhaseSteps) #ms
+print "\nRECEIVER CONFIGURED."
+print "***"
+print "\nINITIALIZING PROG BOARD...\n"
+SpinCore_pp.init_ppg();
+print "\nLOADING PULSE PROG...\n"
+SpinCore_pp.load([
+    ('marker','start',nScans),
+    ('phase_reset',1),
+    ('pulse',p90,0.0),
+    #('pulse',p90,'ph1',0.0),
+    #('delay',tau),
+    #('pulse',2.0*p90,'ph2',0.0),
+    #('delay',transient),
+    #('acquire',acq_time),
+    ('delay',repetition),
+    ('jumpto','start')
+    ])
+print "\nSTOPPING PROG BOARD...\n"
+SpinCore_pp.stop_ppg();
+print "\nRUNNING BOARD...\n"
+for x in xrange(4):
+    print x
+    SpinCore_pp.runBoard();
+SpinCore_pp.stopBoard();
+                        
+#data_length = 2*nPoints*nEchoes*nPhaseSteps
+#raw_data = SpinCore_pp.getData(data_length, nPoints, nEchoes, nPhaseSteps, output_name)
+#print "EXITING..."
+#print "\n*** *** ***\n"
+#raw_data.astype(float)
+#data = []
+## according to JF, this commented out line
+## should work same as line below and be more effic
+##data = raw_data.view(complex128)
+#data[::] = complex128(raw_data[0::2]+1j*raw_data[1::2])
+#print "COMPLEX DATA ARRAY LENGTH:",shape(data)[0]
+#print "RAW DATA ARRAY LENGTH:",shape(raw_data)[0]
+#dataPoints = float(shape(data)[0])
+#time_axis = linspace(0.0,nEchoes*nPhaseSteps*acq_time*1e-3,dataPoints)
+#data = nddata(array(data),'t')
+#data.setaxis('t',time_axis).set_units('t','s')
+#data.name('signal')
+#save_file = True
+#while save_file:
+#    try:
+#        print "SAVING FILE..."
+#        data.hdf5_write(date+'_'+output_name+'.h5')
+#        print "Name of saved data",data.name()
+#        print "Units of saved data",data.get_units('t')
+#        print "Shape of saved data",ndshape(data)
+#        save_file = False
+#    except Exception as e:
+#        print e
+#        print "FILE ALREADY EXISTS."
+#        save_file = False
+#fl.next('raw data')
+#fl.plot(data.real,alpha=0.8)
+#fl.plot(data.imag,alpha=0.8)
+#data.ft('t',shift=True)
+#fl.next('FT raw data')
+#fl.plot(data.real,alpha=0.8)
+#fl.plot(data.imag,alpha=0.8)
+#fl.show()
+#
