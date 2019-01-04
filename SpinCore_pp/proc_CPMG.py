c3e463ac35a70fc0d07f2336acb9a36bdb04dfb6
aabeaton <aabeaton@syr.edu>
2019-01-04 11:54:40 -0500

    Processed properly timed phase cycled CPMG

--- SpinCore_pp/proc_CPMG.py
+++ SpinCore_pp/proc_CPMG.py
@@ -1,6 +1,5 @@
 from pyspecdata import *
-from scipy.optimize import leastsq
-from scipy.optimize import minimize 
+from scipy.optimize import leastsq,minimize,basinhopping
 fl = figlist_var()
 for date,id_string in [
         ('190103','CPMG_ph3')
@@ -8,151 +7,165 @@ for date,id_string in [
     nPoints = 128
     nEchoes = 32
     nPhaseSteps = 4
-    p90 = 0.8*1e-6
+    SW_kHz = 64.0
     filename = date+'_'+id_string+'.h5'
     nodename = 'signal'
     s = nddata_hdf5(filename+'/'+nodename,
             directory = getDATADIR(
                 exp_type = 'test_equip'))
     s.set_units('t','s')
-    orig_t = s.getaxis('t')
-    fl.next(id_string+' raw data')
+    fl.next(id_string+'raw data ')
     fl.plot(s.real,alpha=0.4)
     fl.plot(s.imag,alpha=0.4)
-    t2_axis = linspace(0,s.getaxis('t')[nPoints],nPoints)
+    fl.plot(abs(s),':',c='k',alpha=0.4)
+    orig_t = s.getaxis('t')
+    p90_s = 0.8*1e-6
+    transient_s = 500.0*1e-6
+    acq_time_s = orig_t[nPoints]
+    tau_s = transient_s + acq_time_s*0.5
+    pad_s = 2.0*tau_s - transient_s - acq_time_s - 2.0*p90_s
+    tE_s = 2.0*p90_s + transient_s + acq_time_s + pad_s
+    print "ACQUISITION TIME:",acq_time_s,"s"
+    print "TAU DELAY:",tau_s,"s"
+    print "TWICE TAU:",2.0*tau_s,"s"
+    print "ECHO TIME:",tE_s,"s"
+    t2_axis = linspace(0,acq_time_s,nPoints)
+    tE_axis = r_[1:nEchoes+1]*tE_s
     s.setaxis('t',None)
-    s.chunk('t',['ph1','nEchoes','t2'],[nPhaseSteps,nEchoes,-1])
+    s.chunk('t',['ph1','tE','t2'],[nPhaseSteps,nEchoes,-1])
     s.setaxis('ph1',r_[0.,1.,2.,3.]/4)
-    s.setaxis('nEchoes',r_[1:nEchoes+1])
+    #tE_axis = r_[1:nEchoes+1]*tE_s
+    s.setaxis('tE',tE_axis)
     s.setaxis('t2',t2_axis)
-    fl.next(id_string+' chunked, no ft')
+    fl.next(id_string+'raw data - chunking')
     fl.image(s)
-    fl.next(id_string+' chunked, no ft - cropped log')
-    fl.image(s.C.cropped_log())
+    s.ft('t2', shift=True)
+    fl.next(id_string+'raw data - chunking ft')
+    fl.image(s)
+    clock_correction = -10.51/6 # radians per second
+    s * exp(-1j*s.fromaxis('tE')*clock_correction)
+    fl.next(id_string+'raw data - chunking, clock correction ft')
+    fl.image(s)
+    s.ift('t2')
+    fl.next(id_string+'raw data - chunking, clock correction')
+    fl.image(s)
     s.ft(['ph1'])
     fl.next(id_string+' image plot coherence')
     fl.image(s)
     fl.next(id_string+' image plot coherence -- ft')
-    s.ft('t2', shift=True)
+    s.ft('t2')
     fl.image(s)
     s.ift('t2')
-    phasing = False
-    #{{{ code for determining preliminary phase corrections
-    if phasing:
-        sample = s['ph1',1]['nEchoes',0].C
-        fl.next('phase plot')
-        fl.plot(sample.real,',',alpha=0.4,label='real')
-        fl.plot(sample.imag,',',alpha=0.4,label='imag')
-        sample.ft('t2',shift=True)
-        f_axis = sample.getaxis('t2')
-        sample.ift('t2')
-        SWH = diff(r_[f_axis[0],f_axis[-1]])[0]
-        ph0 = 51.3e-3#+0.3e-3
-        ph1 = 277e-6#-5.8e-6
-        ph0_corr = exp(-1j*2*pi*ph0)
-        ph1_corr = 1j*2*pi*ph1
-        sample.ft('t2')
-        sample *= ph0_corr
-        sample *= exp(sample.fromaxis('t2')*ph1_corr)
-        sample *= exp(1j*0.5*pi)
-        sample.ift('t2')
-        fl.next('phase plot')
-        fl.plot(sample.real,alpha=0.4,label='real ph')
-        fl.plot(sample.imag,alpha=0.4,label='imag ph')
-        gen_cost = False
-        if gen_cost:
-            sample.ft('t2')
-            N = 100
-            dw = 100
-            x = nddata(r_[-0.3:0.3:N*1j],'phi0').set_units('phi0','cyc')
-            phi0 = exp(-1j*2*pi*x)
-            x = nddata(r_[-3e-1*dw/SWH/2:3e-1*dw/SWH/2:N*1j],'phi1').set_units('phi1','s')
-            phi1 = -1j*2*pi*x
-            sample *= phi0
-            sample *= exp(phi1*sample.fromaxis('t2'))
-            sample_absr = sample.C
-            sample_absr.data = abs(sample_absr.data.real)
-            sample_absr.sum('t2')
-            fl.next('abs real cost')
-            fl.image(sample_absr)
-            fl.show();quit()
-        fl.show();quit()
-    #}}}
-    ph0 = 51.3e-3
-    ph1 = 277e-6
-    ph0_corr = exp(-1j*2*pi*ph0)
-    ph1_corr = 1j*2*pi*ph1
-    s.ft('t2')
-    s *= ph0_corr
-    s *= exp(s.fromaxis('t2')*ph1_corr)
-    s *= exp(1j*0.5*pi)
-    s.ift('t2')
-    approx_echo_center = abs(s['ph1',r_[1,3]]).run(sum,'nEchoes').run(sum,'ph1').argmax('t2').data.item()
-    s.setaxis('t2',lambda x: x-approx_echo_center)
-    interleaved = ndshape([2,16,128],['evenodd','nEchoes','t2']).alloc()
-    interleaved.setaxis('evenodd',r_[0:3])
-    interleaved.setaxis('nEchoes',r_[1:nEchoes/2+1])
-    interleaved.setaxis('t2',t2_axis)
-    interleaved['evenodd',0] = s['ph1',1]['nEchoes',0::2].C.run(conj)
-    interleaved['evenodd',1] = s['ph1',-1]['nEchoes',1::2].C
-    interleaved.setaxis('t2',s.getaxis('t2'))
-    fl.next('interleaved')
+    even_echo_center = abs(s)['ph1',1]['tE',0].argmax('t2').data.item()
+    odd_echo_center = abs(s)['ph1',-1]['tE',1].argmax('t2').data.item()
+    print "EVEN ECHO CENTER:",even_echo_center,"s"
+    print "ODD ECHO CENTER:",odd_echo_center,"s"
+    s.setaxis('t2',lambda x: x-even_echo_center)
+    s.rename('tE','nEchoes').setaxis('nEchoes',r_[1:nEchoes+1])
+    fl.next('check center before interleaving')
+    fl.image(s)
+    interleaved = ndshape(s)
+    interleaved['ph1'] = 2
+    interleaved['nEchoes'] /= 2
+    interleaved = interleaved.rename('ph1','evenodd').alloc()
+    interleaved.copy_props(s).setaxis('t2',s.getaxis('t2').copy()).set_units('t2',s.get_units('t2'))
+    interleaved['evenodd',0] = s['ph1',1]['nEchoes',0::2].C.run(conj)['t2',::-1]
+    interleaved['evenodd',1] = s['ph1',-1]['nEchoes',1::2]
+    interleaved.ft('t2')
+    fl.next('even and odd')
     fl.image(interleaved)
-    interleaved.ft('t2', shift=True)
     phdiff = interleaved['evenodd',1]/interleaved['evenodd',0]*abs(interleaved['evenodd',0])
-    fl.next('ph diff')
+    fl.next('phdiff')
     fl.image(phdiff)
     phdiff *= abs(interleaved['evenodd',1])
-    f_axis = interleaved.getaxis('t2')
+    f_axis = interleaved.fromaxis('t2')
     def costfun(firstorder):
         phshift = exp(-1j*2*pi*f_axis*firstorder)
         return -1*abs((phdiff * phshift).data[:].sum())
     sol = minimize(costfun, ([0],),
             method='L-BFGS-B',
-            bounds=((-1e-3,1e-3),))
+            bounds=((-1e-3,1e-3),)
+            )
     firstorder = sol.x[0]
     phshift = exp(-1j*2*pi*f_axis*firstorder)
     phdiff_corr = phdiff.C
     phdiff_corr *= phshift
     zeroorder = phdiff_corr.data[:].sum().conj()
     zeroorder /= abs(zeroorder)
-    phdiff_corr *= zeroorder
-    interleaved['evenodd',1] *= zeroorder
-    interleaved['evenodd',1] *= exp(-1j*2*pi*interleaved['evenodd',1].fromaxis('t2')*firstorder)
-    interleaved.ift('t2')
-    interleaved.reorder('evenodd',first=False)
-    interleaved.smoosh(['nEchoes','evenodd'],noaxis=True)
-    interleaved.reorder('t2',first=False)
+    fl.next('phdiff -- corrected')
+    fl.image(phdiff_corr)
+    print "Relative phase shift (for interleaving) was "        "{:0.1f}\us and {:0.1f}$^\circ$".format(
+                firstorder/1e-6,angle(zeroorder)/pi*180)
+    interleaved['evenodd',1] *= zeroorder*phshift
+    interleaved.smoosh(['nEchoes','evenodd'],noaxis=True).reorder('t2',first=False)
     interleaved.setaxis('nEchoes',r_[1:nEchoes+1])
-    interleaved.setaxis('t2',s.getaxis('t2')).set_units('t2','s')
-    interleaved.reorder('t2',first=True)
-    fl.next('interleaved, smooshed')
+    interleaved.ift('t2')
+    fl.next('interleaved')
     fl.image(interleaved)
     interleaved.ft('t2')
-    fl.next('interleaved, smooshed ft')
+    fl.next('interleaved -- ft')
     fl.image(interleaved)
+    f_axis = interleaved.fromaxis('t2')
+    def costfun(p):
+        zeroorder_rad,firstorder = p
+        phshift = exp(-1j*2*pi*f_axis*(firstorder*1e-6))
+        phshift *= exp(-1j*2*pi*zeroorder_rad)
+        corr_test = phshift * interleaved
+        return (abs(corr_test.data.imag)**2)[:].sum()
+    iteration = 0
+    def print_fun(x, f, accepted):
+        global iteration
+        iteration += 1
+        print (iteration, x, f, int(accepted))
+        return
+    sol = basinhopping(costfun, r_[0.,0.],
+            minimizer_kwargs={"method":'L-BFGS-B'},
+            callback=print_fun,
+            stepsize=100.,
+            niter=100,
+            T=1000.
+            )
+    zeroorder_rad, firstorder = sol.x
+    phshift = exp(-1j*2*pi*f_axis*(firstorder*1e-6))
+    phshift *= exp(-1j*2*pi*zeroorder_rad)
+    interleaved *= phshift
+    print "RELATIVE PHASE SHIFT WAS {:0.1f}\us and {:0.1f}$^\circ$".format(
+            firstorder,angle(zeroorder_rad)/pi*180)
+    if interleaved['nEchoes',0].data[:].sum().real < 0:
+        interleaved *= -1
+    fl.next('interleaved -- phased ft')
+    fl.image(interleaved)
+    fl.next('final real data ft')
+    fl.image(interleaved.real)
+    fl.next('final imag data ft')
+    fl.image(interleaved.imag)
+    print ndshape(interleaved)
+    interleaved.reorder('t2',first=True)
+    fl.next('phased echoes, real - ft')
+    fl.plot(interleaved.real)
+    fl.next('phased echoes, imag - ft')
+    fl.plot(interleaved.imag)
+    interleaved = interleaved['t2':(-4e3,4e3)].C
     interleaved.ift('t2')
-    fl.next('plot 1')
-    fl.plot(interleaved['t2':(-0.15e-3,0.23e-3)])
-    data = interleaved['t2':(-0.15e-3,0.23e-3)].C.sum('t2')
-    fl.next('Plot')
-    echo_spacing = r_[0:32.0*(5.1+0.5)*1e-3+2.0*p90:32j]
-    x = echo_spacing
+    fl.next('phased echoes, real')
+    fl.plot(interleaved.real)
+    fl.next('phased echoes, imag')
+    fl.plot(interleaved.imag)
+    interleaved.rename('nEchoes','tE').setaxis('tE',tE_axis)
+    data = interleaved.C.sum('t2')
+    fl.next('Fit decay')
+    x = tE_axis 
     ydata = data.data.real
     ydata /= max(ydata)
-    fl.plot(x,ydata, '.', alpha=0.4, human_units=False)
+    fl.plot(x,ydata, '.', alpha=0.4, label='data', human_units=False)
     fitfunc = lambda p, x: exp(-x/p[0])
     errfunc = lambda p_arg, x_arg, y_arg: fitfunc(p_arg, x_arg) - y_arg
     p0 = [0.2]
     p1, success = leastsq(errfunc, p0[:], args=(x, ydata))
     x_fit = linspace(x.min(),x.max(),5000)
-    fl.plot(x_fit, fitfunc(p1, x_fit),':',human_units=False)
+    fl.plot(x_fit, fitfunc(p1, x_fit),':', label='fit (T2 = %0.2f ms)'%(p1[0]*1e3), human_units=False)
+    xlabel('t (sec)')
+    ylabel('Intensity')
     T2 = p1[0]
-    print "T2:",T2
-    fl.show();quit()
-    print ndshape(interleaved);quit()
-    data_concat = nddata(concatenate(interleaved['nEchoes',:].data),['t2'])
-    data_concat.setaxis('t2',orig_t[len(data_concat.data)])
-    fl.next('Echoes')
-    fl.plot(data_concat)
+    print "T2:",T2,"s"
 fl.show();quit()
