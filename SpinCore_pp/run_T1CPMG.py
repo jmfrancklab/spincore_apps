48b1bd4bcd8d88dc4bca2e198e203f92ad6d9998
Alec Beaton (SU Network) <aabeaton@syr.edu>
2019-01-16 21:27:53 -0500

    Re-captured (overwrrote existing data) with correct ADC offset for T1CPMG datasets

--- SpinCore_pp/run_T1CPMG.py
+++ SpinCore_pp/run_T1CPMG.py
@@ -40,9 +40,9 @@ import SpinCore_pp
 import time
 fl = figlist_var()
 date = '190116'
-output_name = 'T1CPMG_2'
+output_name = 'T1CPMG_1'
 clock_correction = -10.51/6 # clock correction in radians per second (additional phase accumulated after phase_reset)
-adcOffset = 50
+adcOffset = 49
 carrierFreq_MHz = 14.46 
 tx_phases = r_[0.0,90.0,180.0,270.0]
 amplitude = 1.0
@@ -67,26 +67,26 @@ if not phase_cycling:
     nPhaseSteps = 1 
 data_length = 2*nPoints*nEchoes*nPhaseSteps
 # NOTE: Number of segments is nEchoes * nPhaseSteps
-# vd_list = r_[9.5e1,5e3,
-#        5e4,6e4,6.5e4,9.5e4,1e5,1.1e5,1.4e5,1.5e5,1.7e5,2e5,1e6]
-vd_list = r_[1e3,
-        3e3,
-        5e3,
-        7e3,
-        9e3,
-        1e4,
-        3e4,
-        5e4,
-        7e4,
-        9e4,
-        1e5,
-        3e5,
-        7e5,
-        9e5,
-        1e6,
-        3e6,
-        5e6,
-        6e6]
+vd_list = r_[9.5e1,5e3,
+       5e4,6e4,6.5e4,9.5e4,1e5,1.1e5,1.4e5,1.5e5,1.7e5,2e5,1e6]
+#vd_list = r_[1e3,
+#        3e3,
+#        5e3,
+#        7e3,
+#        9e3,
+#        1e4,
+#        3e4,
+#        5e4,
+#        7e4,
+#        9e4,
+#        1e5,
+#        3e5,
+#        7e5,
+#        9e5,
+#        1e6,
+#        3e6,
+#        5e6,
+#        6e6]
 for index,val in enumerate(vd_list):
     vd = val
     print "***"
