081eb0d080c8e6ded6944cb3cf1e54c71be9c8c4
aabeaton <aabeaton@syr.edu>
2019-02-04 12:29:49 -0500

    Processing to compare field off vs field on

--- SpinCore_pp/proc_MWE.py
+++ SpinCore_pp/proc_MWE.py
@@ -1,12 +1,17 @@
 from pyspecdata import *
 fl = figlist_var()
 for date,id_string,PW in [
-        ('190201','MWE_1','10.44'), # 100 transients, theta = 10.44 us, d1 = 120 us
+        ('190131','MWE_B0ON','2.0'), # 100 transients, theta = 10.44 us, d1 = 120 us
+        ('190131','MWE_B0OFF','2.0'), # 100 transients, theta = 10.44 us, d1 = 120 us
         ]:
     SW_kHz = 20.0
     nPoints = 64
     filename = date+'_'+id_string+'.h5'
-    id_string = id_string.replace('MWE_','Example ') # for plotting purposes
+    if 'B0ON' in id_string:
+        id_string = 'Field On'
+    if 'B0OFF' in id_string:
+        id_string = 'Field Off'
+    #id_string = id_string.replace('MWE_','Example ') # for plotting purposes
     nodename = 'transients'
     s = nddata_hdf5(filename+'/'+nodename,
             directory = getDATADIR(
@@ -15,11 +20,12 @@ for date,id_string,PW in [
     trans_no = len(s.getaxis('trans_no'))
     s.setaxis('trans_no',r_[1:trans_no+1])
     #fl.next('Raw data: %s, Pulse Width = %s $\mu$s'%(id_string,PW))
-    #fl.image(s)
-    fl.next('real')
-    s.real.waterfall()
-    fl.next('imag')
-    s.imag.waterfall()
-    fl.next('abs')
-    abs(s).waterfall()
+    fl.next('Raw data: %s, Pulse Width = %s $\mu$s'%(id_string,PW))
+    fl.image(s)
+    #fl.next('real')
+    #s.real.waterfall()
+    #fl.next('imag')
+    #s.imag.waterfall()
+    #fl.next('abs')
+    #abs(s).waterfall()
 fl.show();quit()
