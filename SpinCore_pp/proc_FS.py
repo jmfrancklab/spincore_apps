d9a50d553e9a87b1408c9fc5d51dc19d4cf925d2
Alec Beaton (SU Network) <aabeaton@syr.edu>
2019-02-21 12:22:38 -0500

    Processing field sweep data from last night

--- SpinCore_pp/proc_FS.py
+++ SpinCore_pp/proc_FS.py
@@ -1,14 +1,6 @@
 from pyspecdata import *
 fl = figlist_var()
 for date,id_string in [
-        #('190220','FS_6_on'),
-        #('190220','FS_6_off'),
-        #('190220','FS_6_on_2'),
-        #('190220','FS_6_on_3'),
-        #('190220','FS_6_on_4'),
-        #('190220','FS_8'),
-        #('190220','FS_9'),
-        #('190220','FS_10'),
         ('190220','FS_11'),
         ]:
     filename = date+'_'+id_string+'.h5'
@@ -32,70 +24,16 @@ for date,id_string in [
     fl.next('raw, chunk'+id_string)
     fl.image(s)
     s.ft(['ph2','ph1'])
+    print ndshape(s)
     fl.next('raw ft ph, chunk'+id_string)
     fl.image(s)
     s.ft('t2',shift=True)
-    fl.next('raw ft ph, chunk freq'+id_string)
-    fl.image(s['t2':(-20e3,20e3)])
-    s = s['ph2',0]['ph1',1].C
-    fl.next('coherence')
-    fl.image(s['t2':(-20e3,20e3)])
-    fl.show();quit()
-    #s.ift('t2')
-    #fl.show();quit()
-    #s = s['ph2',0]['ph1',1].C
-    #fl.next('coh ch, time'+id_string)
-    #fl.image(abs(s['t2':(1.8e-3,2.2e-3)]))
-    #s.ft('t2')
-    #fl.next('coh ch, freq'+id_string)
-    #fl.image(abs(s))
-    #s.ift('t2')
-    #fl.next('data'+id_string)
-    #fl.plot(s.real['field',-3],alpha=0.5)
-    #fl.plot(s.imag['field',-3],alpha=0.5)
+    s_offset = s.C
+    s_offset.setaxis('t2', lambda f: f/(gammabar_H)*1e4)
+    s_offset.set_units('t2','G')
+    s_offset.rename('t2',r'$\frac{\Omega}{\gamma_{H}}$')
+    fl.next('image, all coherence channels')
+    fl.image(s_offset[r'$\frac{\Omega}{\gamma_{H}}$':(-3,3)])
+    fl.next('image, $\Delta c_{1}$ = 1, $\Delta c_{2}$ = 0')
+    fl.image(s_offset[r'$\frac{\Omega}{\gamma_{H}}$':(-3,3)]['ph2',0]['ph1',1])
 fl.show();quit()
-    ###s.ft('t',shift=True)
-    ###time_shift = False
-    ###if time_shift:
-    ###offset_plot_shift = s.C
-    ###offset_plot_shift *= exp(1j*2*pi*s.fromaxis('t')*2e-3)
-    ###offset_plot_shift.setaxis('t', lambda x: x/(gammabar_H)*1e4).set_units('t','G')
-    ###offset_plot_shift.rename('t',r'$\frac{\Omega}{\gamma_{H}}$')
-    ###offset_plot = s.C
-    #### gammabar_H is in units of Hz/T
-    ###offset_plot.setaxis('t', lambda x: x/(gammabar_H)*1e4).set_units('t','G')
-    ###offset_plot.rename('t',r'$\frac{\Omega}{\gamma_{H}}$')
-    ###fl.next('offset')
-    ###fl.plot(offset_plot['field',2],alpha=0.35,label=r'$B_{0} = %0.1f G$ (raw)'%offset_plot.getaxis('field')[2])
-    ###if time_shift:
-    ###fl.plot(offset_plot_shift['field',2],alpha=0.35,label=r'$B_{0} = %0.1f G$ (time shift)'%offset_plot.getaxis('field')[2])
-    ###print ndshape(offset_plot)
-    ###offset_plot.reorder('field', first=True)
-    ###fl.next('sweep, image plot abs (offset)')
-    ###fl.image(abs(offset_plot))
-    ###fl.next('sweep, image plot (offset)')
-    ###fl.image(offset_plot)
-    ###if time_shift:
-    ###offset_plot_shift.reorder('field', first=True)
-    ###fl.next('sweep, image plot abs (offset, time shift)')
-    ###fl.image(abs(offset_plot_shift))
-    ###fl.next('sweep, image plot (offset, time shift)')
-    ###fl.image(offset_plot_shift)
-    ###s.ift('t')
-    ###s.reorder('t', first=False)
-    ###fl.next('sweep, image plot abs (time)')
-    ####fl.image(abs(s['t':(0,4e-3)]))
-    ###fl.image(abs(s))
-    ###fl.next('sweep, image plot (time)')
-    ####fl.image(s['t':(0,4e-3)])
-    ###fl.image(s)
-    ###fl.next('data')
-    ###fl.plot(s.real['field',-3])
-    ###fl.plot(s.imag['field',-3])
-    ###fl.show();quit()
-    ###s.ft('t')
-    ###print ndshape(s)
-    ###s.reorder('t',first=True)
-    ###print ndshape(s)
-    ####fl.plot(abs(s)['field':3418.0])
-    ###fl.show()
