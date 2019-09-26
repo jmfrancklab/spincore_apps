17bc28dc6149e3b4ad38011e01b41d89f7616e72
Alec Beaton (SU Network) <aabeaton@syr.edu>
2019-09-26 09:50:02 -0400

    Testing out NMR since last experiments were over a month ago on sample of 150 uM TEMPOL

--- SpinCore_pp/proc_Hahn_echo.py
+++ SpinCore_pp/proc_Hahn_echo.py
@@ -2,12 +2,12 @@ from pyspecdata import *
 from scipy.optimize import leastsq,minimize,basinhopping
 fl = figlist_var()
 for date,id_string in [
-        ('190630','test'),
+        ('190926','echo_1'),
         ]:
     nEchoes = 1
     nPhaseSteps = 1
-    SW_kHz = 15.0
-    nPoints = 256
+    SW_kHz = 3.0
+    nPoints = 128
     filename = date+'_'+id_string+'.h5'
     nodename = 'signal'
     s = nddata_hdf5(filename+'/'+nodename,
@@ -15,7 +15,6 @@ for date,id_string in [
                 exp_type = 'test_equip'))
     s.set_units('t','s')
     print s.get_prop('acq_params')
-    quit()
     fl.next('raw data')
     fl.plot(s.real,alpha=0.4)
     fl.plot(s.imag,alpha=0.4)
