e746f97080085a547c7e9a25f03d8a0cab3abb47
John Franck (laptop) <jmfranck@syr.edu>
2019-10-21 23:41:38 -0400

    done with hermitian cost function

--- SpinCore_pp/hermitian_phasing.py
+++ SpinCore_pp/hermitian_phasing.py
@@ -1,10 +1,10 @@
 from pyspecdata import *
 from scipy.optimize import leastsq,minimize,basinhopping
 class flv(figlist_var):
-    def show_complex(fl,d):
-        fl.plot(abs(s),'k',alpha=0.5)
-        fl.plot(s.real,'b',alpha=0.5)
-        fl.plot(s.imag,'g',alpha=0.5)
+    def show_complex(fl,d,**kwargs):
+        fl.plot(abs(d),'k',alpha=0.5,**kwargs)
+        fl.plot(d.real,'b',alpha=0.5,**kwargs)
+        fl.plot(d.imag,'g',alpha=0.5,**kwargs)
         return
 fl = flv()
 for date,id_string in [
@@ -22,7 +22,7 @@ for date,id_string in [
     nPhaseSteps = s.get_prop('acq_params')['nPhaseSteps']
     SW_kHz = s.get_prop('acq_params')['SW_kHz']
     s.reorder('t',first=True)
-    s.chunk('t',['ph2','ph1','t2'],[2,4,-1])
+    s.chunk('t',['ph2','ph1','t2'],[2,4,-1]).set_units('t2','s')
     s.setaxis('ph2',r_[0.,2.]/4)
     s.setaxis('ph1',r_[0.,1.,2.,3.]/4)
     s.reorder('t2',first=False)
@@ -49,7 +49,7 @@ for date,id_string in [
     s.ift('t2')
     # }}}
     fl.next('frequency filtered')
-    fl.show_complex(s)
+    fl.show_complex(s,human_units=False)
     max_data = abs(s.data).max()
     pairs = s.contiguous(lambda x: abs(x) > max_data*0.5)
     longest_pair = diff(pairs).argmax()
@@ -65,11 +65,17 @@ for date,id_string in [
     # at this point, I need to get an axis that's in register with 0
     # -- this is what gives rise to "zero not in middle" below
     # I think I already have a function to do this
+    s.register_axis({'t2':0})
     fl.next('crude centering')
     fl.show_complex(s)
     max_shift = diff(peak_location).item()/2
-    # {{{ just check signs of shift
+    fl.next('spectrum with crude centering')
+    s_sliced = s['t2':(0,None)].C
+    s_sliced['t2',0] *= 0.5
+    s_sliced.ft('t2')
+    fl.show_complex(s_sliced)
     s.ft('t2')
+    # {{{ just check signs of shift
     s_left = s * exp(+1j*2*pi*max_shift*s.fromaxis('t2'))
     s_right = s * exp(-1j*2*pi*max_shift*s.fromaxis('t2'))
     s.ift('t2')
@@ -92,12 +98,6 @@ for date,id_string in [
         s_shifted = s_shifted['t2',:-1]
     assert s_shifted.getaxis('t2')[s_shifted.getaxis('t2').size//2+1] == 0, 'zero not in the middle! -- does your original axis contain a 0?'
     # }}}
-    fl.next('first')
-    fl.show_complex(s_shifted['shift',0])
-    fl.next('middle')
-    fl.show_complex(s_shifted['shift':0.0])
-    fl.next('last')
-    fl.show_complex(s_shifted['shift',-1])
     # {{{ the middle point, at t=0 must be entirely real
     ph0 = s_shifted['t2':0.0]
     ph0 /= abs(ph0)
@@ -106,4 +106,31 @@ for date,id_string in [
     residual = abs(s_shifted - s_shifted['t2',::-1].runcopy(conj)).sum('t2')
     fl.next('cost function')
     fl.plot(residual)
+    best_shift = residual.argmin('shift').item()
+    s.ft('t2')
+    s *= exp(-1j*2*pi*best_shift*s.fromaxis('t2'))
+    s.ift('t2')
+    ph0 = s['t2':0.0]
+    ph0 /= abs(ph0)
+    s /= ph0
+    fl.next('spectrum with optimized centering')
+    s_sliced = s['t2':(0,None)].C
+    s_sliced['t2',0] *= 0.5
+    s_sliced.ft('t2')
+    fl.show_complex(s_sliced)
+    # {{{ 
+    fl.next("attempt to superimpose left side to add to SNR\ndoesn't work because of T2 decay (?)\n(Illustrates limits of method)")
+    s_sliced = s['t2':(0,None)].C
+    s_leftslice = s['t2':(None,0)]['t2',::-1].run(conj)
+    s_leftslice.setaxis('t2',lambda x: -1*x)
+    print s_sliced.getaxis('t2')[r_[0,-1]]
+    print s_leftslice.getaxis('t2')[r_[0,-1]]
+    fl.show_complex(s_sliced)
+    fl.show_complex(s_leftslice)
+    #assert s_leftslice.getaxis('t2')[0] != 0, "libraries have changed and left slice includes 0"
+    #s_sliced['t2',1:ndshape(s_leftslice)['t2']+1] += s_leftslice
+    #s_sliced['t2',0:ndshape(s_leftslice)['t2']+1] *= 0.5
+    #s_sliced.ft('t2')
+    #fl.show_complex(s_sliced)
+    # }}}
 fl.show()
