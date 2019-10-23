e17dbf898db0442f2848dd9fceec950aff81a72b
John Franck (desktop) <jmfranck@syr.edu>
2019-10-23 15:34:12 -0400

    add R2 correction

--- SpinCore_pp/hermitian_phasing.py
+++ SpinCore_pp/hermitian_phasing.py
@@ -87,10 +87,12 @@ for date,id_string in [
     fl.plot(abs(s_right), label='max right',alpha=0.5)
     # }}}
     shift_t = nddata(r_[-1:1:1000j]*max_shift, 'shift')
+    t2_decay = exp(-s.fromaxis('t2')*nddata(r_[0:1e3:100j],'R2'))
     s.ft('t2')
     s_shifted = s * exp(-1j*2*pi*shift_t*s.fromaxis('t2'))
     s.ift('t2')
     s_shifted.ift('t2')
+    s_shifted /= t2_decay
     s_shifted = s_shifted['t2':(-max_shift,max_shift)]
     # {{{ demand an odd number of points
     print s_shifted.getaxis('t2')[r_[0,ndshape(s_shifted)['t2']//2,ndshape(s_shifted)['t2']//2+1,-1]]
@@ -102,11 +104,17 @@ for date,id_string in [
     ph0 = s_shifted['t2':0.0]
     ph0 /= abs(ph0)
     s_shifted /= ph0
+    s_shifted /= abs(s_shifted).run(max,'t2')
     # }}}
     residual = abs(s_shifted - s_shifted['t2',::-1].runcopy(conj)).sum('t2')
     fl.next('cost function')
-    fl.plot(residual)
-    best_shift = residual.argmin('shift').item()
+    fl.image(residual)
+    fl.plot(residual.C.argmin('shift'),'x')
+    minpoint = residual.argmin()
+    best_shift = minpoint['shift']
+    best_R2 = minpoint['R2']
+    #fl.plot(best_R2,best_shift,'o')
+    # replace following with time shift function
     s.ft('t2')
     s *= exp(-1j*2*pi*best_shift*s.fromaxis('t2'))
     s.ift('t2')
@@ -119,10 +127,11 @@ for date,id_string in [
     s_sliced.ft('t2')
     fl.show_complex(s_sliced)
     # {{{ 
-    fl.next("attempt to superimpose left side to add to SNR\ndoesn't work because of T2 decay (?)\n(Illustrates limits of method)")
+    fl.next("edit superimposition to include a correction for T2 decay")
     s_sliced = s['t2':(0,None)].C
     s_leftslice = s['t2':(None,0)]['t2',::-1].run(conj)
     s_leftslice.setaxis('t2',lambda x: -1*x)
+    print "check time slices"
     print s_sliced.getaxis('t2')[r_[0,-1]]
     print s_leftslice.getaxis('t2')[r_[0,-1]]
     fl.show_complex(s_sliced)
