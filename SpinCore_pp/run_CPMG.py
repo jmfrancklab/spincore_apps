5830639f8b465b34d7467e5259c797497f0f1e79
Alec Beaton (SU Network) <aabeaton@syr.edu>
2020-01-15 18:44:03 -0500

    Determining proper 90 time

--- SpinCore_pp/run_CPMG.py
+++ SpinCore_pp/run_CPMG.py
@@ -40,12 +40,12 @@ import SpinCore_pp
 fl = figlist_var()
 
 date = '200115'
-output_name = 'CPMG_17'
+output_name = 'CPMG_20_3p5'
 adcOffset = 45
 carrierFreq_MHz = 14.898122
 tx_phases = r_[0.0,90.0,180.0,270.0]
 amplitude = 1.0
-p90 = 3.3
+p90 = 3.5
 deadtime = 100.0
 repetition = 15e6
 deblank = 1.0
@@ -60,9 +60,6 @@ pad_start = tau_extra - deadtime
 pad_end = tau_extra - deblank*2 # marker + deblank
 twice_tau = deblank + 2*p90 + deadtime + pad_start + acq_time*1e3 + pad_end + marker
 tau1 = twice_tau/2.0
-#tau1 = 2*p90 - (2*p90)/pi + pad_start + deadtime + 0.5*acq_time*1e3 - deblank - (2*p90)/pi
-
-print "ACQUISITION TIME:",acq_time,"ms"
 
 nScans = 4
 nEchoes = 64
--- SpinCore_pp/run_CPMG_calib.py
+++ SpinCore_pp/run_CPMG_calib.py
@@ -39,31 +39,35 @@ from numpy import *
 import SpinCore_pp 
 fl = figlist_var()
 
-date = '200110'
-output_name = 'CPMG_calib_1'
-adcOffset = 46
-carrierFreq_MHz = 14.898111
+date = '200115'
+output_name = 'CPMG_calib_3'
+adcOffset = 45
+carrierFreq_MHz = 14.898122
 tx_phases = r_[0.0,90.0,180.0,270.0]
 amplitude = 1.0
-deadtime = 60.0
-repetition = 10e6
+deadtime = 100.0
+repetition = 15e6
+deblank = 1.0
+marker = 1.0
 
 SW_kHz = 4.0
 nPoints = 128
-
-deblank = 1.0
 acq_time = nPoints/SW_kHz # ms
-tau_adjust = 1000.
-tau = deadtime + acq_time*1e3*0.5 + tau_adjust
-nScans = 1
+
+tau_extra = 200.0 # us, must be more than deadtime and more than deblank
+pad_start = tau_extra - deadtime
+pad_end = tau_extra - deblank*2 # marker + deblank
+
+nScans = 4
 nEchoes = 64
+
 phase_cycling = True
 if phase_cycling:
     nPhaseSteps = 2
 if not phase_cycling:
     nPhaseSteps = 1 
 data_length = 2*nPoints*nEchoes*nPhaseSteps
-p90_range = linspace(2.0,12.0,30)#,endpoint=False)
+p90_range = linspace(3.0,4.0,5)#,endpoint=False)
 #{{{ setting acq_params dictionary
 acq_params = {}
 acq_params['adcOffset'] = adcOffset
@@ -74,21 +78,21 @@ acq_params['nEchoes'] = nEchoes
 acq_params['p90_us'] = p90_range
 acq_params['deadtime_us'] = deadtime
 acq_params['repetition_us'] = repetition
+acq_params['deblank_us'] = deblank
+acq_params['tau_extra_us'] = tau_extra
+acq_params['pad_start_us'] = pad_start
+acq_params['pad_end_us'] = pad_end
+acq_params['marker_us'] = marker
 acq_params['SW_kHz'] = SW_kHz
 acq_params['nPoints'] = nPoints
-acq_params['tau_adjust_us'] = tau_adjust
-acq_params['deblank_us'] = deblank
-acq_params['tau_us'] = tau
 if phase_cycling:
     acq_params['nPhaseSteps'] = nPhaseSteps
 #}}}
 # NOTE: Number of segments is nEchoes * nPhaseSteps
 for index,val in enumerate(p90_range):
     p90 = val # us
-    pad = 2.0*tau - deadtime - acq_time*1e3 - 2.0*p90 - deblank
-    print "ACQUISITION TIME:",acq_time,"ms"
-    print "TAU DELAY:",tau,"us"
-    print "PAD DELAY:",pad,"us"
+    twice_tau = deblank + 2*p90 + deadtime + pad_start + acq_time*1e3 + pad_end + marker
+    tau1 = twice_tau/2.0
     print "***"
     print "INDEX %d - 90 TIME %f"%(index,val)
     print "***"
@@ -104,19 +108,21 @@ for index,val in enumerate(p90_range):
                 ('phase_reset',1),
                     ('delay_TTL',deblank),
                     ('pulse_TTL',p90,'ph1',r_[0,2]),
-                    ('delay',tau),
+                    ('delay',tau1),
                     ('delay_TTL',deblank),
                     ('pulse_TTL',2.0*p90,1),
                     ('delay',deadtime),
+                    ('delay',pad_start),
                     ('acquire',acq_time),
-                    ('delay',pad),
-                    ('marker','echo_label',(nEchoes-1)),
+                    ('delay',pad_end),
+                    ('marker','echo_label',(nEchoes-1)), # 1 us delay
                     ('delay_TTL',deblank),
                     ('pulse_TTL',2.0*p90,1),
                     ('delay',deadtime),
+                    ('delay',pad_start),
                     ('acquire',acq_time),
-                    ('delay',pad),
-                    ('jumpto','echo_label'),
+                    ('delay',pad_end),
+                    ('jumpto','echo_label'), # 1 us delay
                     ('delay',repetition),
                     ('jumpto','start')
                     ])
@@ -126,19 +132,21 @@ for index,val in enumerate(p90_range):
                     ('phase_reset',1),
                     ('delay_TTL',deblank),
                     ('pulse_TTL',p90,0.0),
-                    ('delay',tau),
+                    ('delay',tau1),
                     ('delay_TTL',deblank),
                     ('pulse_TTL',2.0*p90,0.0),
                     ('delay',deadtime),
+                    ('delay',pad_start),
                     ('acquire',acq_time),
-                    ('delay',pad),
-                    ('marker','echo_label',(nEchoes-1)),
+                    ('delay',pad_end),
+                    ('marker','echo_label',(nEchoes-1)), # 1 us delay
                     ('delay_TTL',deblank),
                     ('pulse_TTL',2.0*p90,0.0),
                     ('delay',deadtime),
+                    ('delay',pad_start),
                     ('acquire',acq_time),
-                    ('delay',pad),
-                    ('jumpto','echo_label'),
+                    ('delay',pad_end),
+                    ('jumpto','echo_label'), # 1 us delay
                     ('delay',repetition),
                     ('jumpto','start')
                     ])
