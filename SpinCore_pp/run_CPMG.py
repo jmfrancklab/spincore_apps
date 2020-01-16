84a381b461d5e6662a64e1f152ef3022c312e174
Alec Beaton (SU Network) <aabeaton@syr.edu>
2020-01-15 22:16:08 -0500

    CPMG DNP experiments

--- SpinCore_pp/run_CPMG.py
+++ SpinCore_pp/run_CPMG.py
@@ -40,12 +40,12 @@ import SpinCore_pp
 fl = figlist_var()
 
 date = '200115'
-output_name = 'CPMG_20_3p5'
+output_name = 'CPMG_26_2p7_7'
 adcOffset = 45
-carrierFreq_MHz = 14.898122
+carrierFreq_MHz = 14.898373
 tx_phases = r_[0.0,90.0,180.0,270.0]
 amplitude = 1.0
-p90 = 3.5
+p90 = 2.7
 deadtime = 100.0
 repetition = 15e6
 deblank = 1.0
@@ -55,7 +55,7 @@ SW_kHz = 4.0
 nPoints = 128
 acq_time = nPoints/SW_kHz # ms
 
-tau_extra = 200.0 # us, must be more than deadtime and more than deblank
+tau_extra = 5000.0 # us, must be more than deadtime and more than deblank
 pad_start = tau_extra - deadtime
 pad_end = tau_extra - deblank*2 # marker + deblank
 twice_tau = deblank + 2*p90 + deadtime + pad_start + acq_time*1e3 + pad_end + marker
--- SpinCore_pp/run_CPMG_mw.py
+++ SpinCore_pp/run_CPMG_mw.py
@@ -63,27 +63,28 @@ dB_settings = append(dB_settings,[1.5,0.5,0.25])
 print "adjusted my power list by",len(dB_settings)-len(powers),"to satisfy the 3dB step requirement and the 0.5 dB resolution"
 powers = 1e-3*10**(dB_settings/10.)
 
-date = '200109'
+date = '200115'
 output_name = 'CPMG_DNP_1'
-adcOffset = 48
-carrierFreq_MHz = 14.898970
+adcOffset = 45
+carrierFreq_MHz = 14.898373
 tx_phases = r_[0.0,90.0,180.0,270.0]
 amplitude = 1.0
-p90 = 3.5
-deadtime = 60.0
+p90 = 2.7
+deadtime = 100.0
 repetition = 15e6
+deblank = 1.0
+marker = 1.0
 
 SW_kHz = 4.0
 nPoints = 128
-
-deblank = 1.0
 acq_time = nPoints/SW_kHz # ms
-tau_adjust = 1000.0
-tau = deadtime + acq_time*1e3*0.5 + tau_adjust
-pad = 2.0*tau - deadtime - acq_time*1e3 - 2.0*p90 - deblank
-print "ACQUISITION TIME:",acq_time,"ms"
-print "TAU DELAY:",tau,"us"
-print "PAD DELAY:",pad,"us"
+
+tau_extra = 5000.0 # us, must be more than deadtime and more than deblank
+pad_start = tau_extra - deadtime
+pad_end = tau_extra - deblank*2 # marker + deblank
+twice_tau = deblank + 2*p90 + deadtime + pad_start + acq_time*1e3 + pad_end + marker
+tau1 = twice_tau/2.0
+
 nScans = 4
 nEchoes = 64
 phase_cycling = True
@@ -103,12 +104,14 @@ acq_params['nEchoes'] = nEchoes
 acq_params['p90_us'] = p90
 acq_params['deadtime_us'] = deadtime
 acq_params['repetition_us'] = repetition
+acq_params['deblank_us'] = deblank
+acq_params['tau_extra_us'] = tau_extra
+acq_params['pad_start_us'] = pad_start
+acq_params['pad_end_us'] = pad_end
+acq_params['marker_us'] = marker
+acq_params['tau1_us'] = tau1
 acq_params['SW_kHz'] = SW_kHz
 acq_params['nPoints'] = nPoints
-acq_params['tau_adjust_us'] = tau_adjust
-acq_params['deblank_us'] = deblank
-acq_params['tau_us'] = tau
-acq_params['pad_us'] = pad 
 if phase_cycling:
     acq_params['nPhaseSteps'] = nPhaseSteps
 #}}}
@@ -131,19 +134,21 @@ if phase_cycling:
         ('phase_reset',1),
             ('delay_TTL',deblank),
             ('pulse_TTL',p90,'ph1',r_[0,2]),
-            ('delay',tau),
+            ('delay',tau1),
             ('delay_TTL',deblank),
             ('pulse_TTL',2.0*p90,1),
             ('delay',deadtime),
+            ('delay',pad_start),
             ('acquire',acq_time),
-            ('delay',pad),
-            ('marker','echo_label',(nEchoes-1)),
+            ('delay',pad_end),
+            ('marker','echo_label',(nEchoes-1)), # 1 us delay
             ('delay_TTL',deblank),
             ('pulse_TTL',2.0*p90,1),
             ('delay',deadtime),
+            ('delay',pad_start),
             ('acquire',acq_time),
-            ('delay',pad),
-            ('jumpto','echo_label'),
+            ('delay',pad_end),
+            ('jumpto','echo_label'), # 1 us delay
             ('delay',repetition),
             ('jumpto','start')
             ])
@@ -153,19 +158,21 @@ if phase_cycling:
             ('phase_reset',1),
             ('delay_TTL',deblank),
             ('pulse_TTL',p90,0.0),
-            ('delay',tau),
+            ('delay',tau1),
             ('delay_TTL',deblank),
             ('pulse_TTL',2.0*p90,0.0),
             ('delay',deadtime),
+            ('delay',pad_start),
             ('acquire',acq_time),
-            ('delay',pad),
-            ('marker','echo_label',(nEchoes-1)),
+            ('delay',pad_end),
+            ('marker','echo_label',(nEchoes-1)), # 1 us delay
             ('delay_TTL',deblank),
             ('pulse_TTL',2.0*p90,0.0),
             ('delay',deadtime),
+            ('delay',pad_start),
             ('acquire',acq_time),
-            ('delay',pad),
-            ('jumpto','echo_label'),
+            ('delay',pad_end),
+            ('jumpto','echo_label'), # 1 us delay
             ('delay',repetition),
             ('jumpto','start')
             ])
@@ -234,19 +241,21 @@ with Bridge12() as b:
                 ('phase_reset',1),
                     ('delay_TTL',deblank),
                     ('pulse_TTL',p90,'ph1',r_[0,2]),
-                    ('delay',tau),
+                    ('delay',tau1),
                     ('delay_TTL',deblank),
                     ('pulse_TTL',2.0*p90,1),
                     ('delay',deadtime),
+                    ('delay',pad_start),
                     ('acquire',acq_time),
-                    ('delay',pad),
-                    ('marker','echo_label',(nEchoes-1)),
+                    ('delay',pad_end),
+                    ('marker','echo_label',(nEchoes-1)), # 1 us delay
                     ('delay_TTL',deblank),
                     ('pulse_TTL',2.0*p90,1),
                     ('delay',deadtime),
+                    ('delay',pad_start),
                     ('acquire',acq_time),
-                    ('delay',pad),
-                    ('jumpto','echo_label'),
+                    ('delay',pad_end),
+                    ('jumpto','echo_label'), # 1 us delay
                     ('delay',repetition),
                     ('jumpto','start')
                     ])
@@ -256,19 +265,21 @@ with Bridge12() as b:
                     ('phase_reset',1),
                     ('delay_TTL',deblank),
                     ('pulse_TTL',p90,0.0),
-                    ('delay',tau),
+                    ('delay',tau1),
                     ('delay_TTL',deblank),
                     ('pulse_TTL',2.0*p90,0.0),
                     ('delay',deadtime),
+                    ('delay',pad_start),
                     ('acquire',acq_time),
-                    ('delay',pad),
-                    ('marker','echo_label',(nEchoes-1)),
+                    ('delay',pad_end),
+                    ('marker','echo_label',(nEchoes-1)), # 1 us delay
                     ('delay_TTL',deblank),
                     ('pulse_TTL',2.0*p90,0.0),
                     ('delay',deadtime),
+                    ('delay',pad_start),
                     ('acquire',acq_time),
-                    ('delay',pad),
-                    ('jumpto','echo_label'),
+                    ('delay',pad_end),
+                    ('jumpto','echo_label'), # 1 us delay
                     ('delay',repetition),
                     ('jumpto','start')
                     ])
