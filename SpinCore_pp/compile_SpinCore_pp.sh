a44cba91a66218ada7826016ace7729c4d6a6725
Alec Beaton (SU Network) <aabeaton@syr.edu>
2020-02-27 15:34:14 -0500

    tweaks to swig to upgrade to python 3

--- SpinCore_pp/compile_SpinCore_pp.sh
+++ SpinCore_pp/compile_SpinCore_pp.sh
@@ -3,5 +3,5 @@ echo "about to construct wrapper..."
 swig -python -threads -o SpinCore_pp_wrap.c SpinCore_pp.i
 echo "wrapper constructed."
 echo "about to compile module..."
-gcc -shared -DMS_WIN64 -I$conda_headers -I$spincore -I$numpy -L$conda_libs -L$spincore SpinCore_pp.c SpinCore_pp_wrap.c -lpython27 -lmrispinapi64 -o _SpinCore_pp.pyd
+gcc -shared -DMS_WIN64 -I$conda_headers -I$spincore -I$numpy -L$conda_libs -L$spincore SpinCore_pp.c SpinCore_pp_wrap.c -lpython37 -lmrispinapi64 -o _SpinCore_pp.pyd
 echo "module compiled."
