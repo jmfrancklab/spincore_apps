5f131487b488da30ad9e26a3772b5e1a56d963b2
Alec Beaton (SU Network) <aabeaton@syr.edu>
2020-09-30 22:22:25 -0400

    Running high field hyperfine line for field dependent DNP

--- RPG_tune.c
+++ RPG_tune.c
@@ -46,7 +46,7 @@ int main(int argc, char *argv[])
     ERROR_CATCH( spmri_set_defaults() );
 
     // Carrier Frequency Registers
-    double freq[1] = {14.823231};
+    double freq[1] = {14.976870};
     ERROR_CATCH( spmri_set_frequency_registers( freq, 1 ) );
 
     // Phase Registers
--- SpinCore_pp/run_Hahn_echo.py
+++ SpinCore_pp/run_Hahn_echo.py
@@ -33,9 +33,9 @@ def verifyParams():
     return
 #}}}
 
-output_name = 'TEMPOL_3496p6_28dBm'
+output_name = 'TEMPOL_3533p0_0_again'
 adcOffset = 41
-carrierFreq_MHz = 14.855362
+carrierFreq_MHz = 15.009910
 tx_phases = r_[0.0,90.0,180.0,270.0]
 amplitude = 1.0
 nScans = 1
