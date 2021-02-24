c9d3a3d502467797ce255443d89aa442757a8ac5
Alec Beaton (SU Network) <aabeaton@syr.edu>
2021-02-24 18:10:03 -0500

    Test out different rd

--- SpinCore_pp/run_FID.py
+++ SpinCore_pp/run_FID.py
@@ -31,12 +31,12 @@ def verifyParams():
     return
 #}}}
 
-output_name = '4AT_cap_probe_FID_r1_1'
+output_name = '4AT_cap_probe_FID_r3_1'
 adcOffset = 37
 carrierFreq_MHz = 14.893494
 tx_phases = r_[0.0,90.0,180.0,270.0]
 amplitude = 1.0
-nScans = 1
+nScans = 80
 nEchoes = 1
 phase_cycling = True
 #coherence_pathway = [('ph1',1),('ph2',-2)]
@@ -50,9 +50,9 @@ if not phase_cycling:
 # as this is generally what the SpinCore takes
 # note that acq_time is always milliseconds
 #}}}
-p90 = 4.69
+p90 = 2.345
 deadtime = 10.0
-repetition = 5e6
+repetition = 1.21732e6
 
 SW_kHz = 48
 nPoints = 1024*2
