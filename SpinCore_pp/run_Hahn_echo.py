da22960dd7d76a6db5353720121114ab6793e37b
Alec Beaton (SU Network) <aabeaton@syr.edu>
2021-06-18 19:40:41 -0400

    For hexane RM

--- SpinCore_pp/run_Hahn_echo.py
+++ SpinCore_pp/run_Hahn_echo.py
@@ -50,12 +50,12 @@ def verifyParams():
     return
 #}}}
 
-output_name = '100mM_TEMPO_cap_probe_echo'
-node_name = 'echo_1'
-adcOffset = 32
+output_name = '50mM_4AT_D2O_AOT_w11_cap_probe_echo'
+node_name = 'echo_B0_3490p55_34dBm'
+adcOffset = 30
 
-user_sets_Freq = False
-user_sets_Field = False
+user_sets_Freq = True
+user_sets_Field = False 
 
 #{{{ set field here
 if user_sets_Field:
@@ -65,19 +65,19 @@ if user_sets_Field:
 #}}}
 #{{{let computer set field
 if not user_sets_Field:
-    desired_B0 = 3529.55
+    desired_B0 = 3490.55
     with xepr() as x:
         true_B0 = x.set_field(desired_B0)
         print("My field in G is %f"%true_B0)
 #}}}
 #{{{ set frequency here
 if user_sets_Freq:
-    carrierFreq_MHz = 14.889463
+    carrierFreq_MHz = 14.830220
     print("My frequency in MHz is",carrierFreq_MHz)
 #}}}zaa
 #{{{ let computer set frequency
 if not user_sets_Freq:
-    gamma_eff = (14.889463/3504.55)
+    gamma_eff = (14.817352/3487.6)
     carrierFreq_MHz = gamma_eff*true_B0
     print("My frequency in MHz is",carrierFreq_MHz)
 #}}}
@@ -100,7 +100,7 @@ if not phase_cycling:
 #}}}
 p90 = 4.69
 deadtime = 10
-repetition = 3e6
+repetition = 15e6
 
 SW_kHz = 24
 nPoints = 1024*2
