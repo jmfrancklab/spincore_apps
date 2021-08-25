ac1d765d83df7e48f53d6e9a25fef72164415215
Alex <guinneak198@github.com>
2021-08-25 15:54:12 -0400

    working on replicating original error

--- examples/save_data_example.py
+++ examples/save_data_example.py
@@ -1,14 +1,13 @@
 from numpy import *
+from numpy.random import rand
 from pyspecdata import *
 from Instruments import *
 import os
 import sys
 import time
+import random
 long_delay = time.sleep(5)
-short_delay = time.sleep(0.1)
-#from SpinCore_pp.power_helper import gen_powerlist
-#{{{ params for Bridge 12/power
-"Helper functions for dealing with powers"
+short_delay = time.sleep(1)
 def gen_powerlist(max_power, steps, min_dBm_step=0.5):
     "generate a list of (roughly) evenly spaced powers up to max_power"
     lin_steps = steps
@@ -28,28 +27,31 @@ def gen_powerlist(max_power, steps, min_dBm_step=0.5):
                     "can't request %d steps between 0 and %f W without going"
                     "below %f a step?")%(steps,max_power,min_dBm_step)
     return dB_settings
-dB_settings = gen_powerlist(4,25) 
+#{{{ params for Bridge 12/power
+dB_settings = gen_powerlist(4,30.0)
+print(dB_settings)
+quit()
+powers =1e-3*10**(dB_settings/10.)
 #}}}
 #{{{ time of pulse prog
-print("sleep")
-long_delay
 #}}}
-print("after sleep")
 #{{{where error occurs
 meter_powers = zeros_like(dB_settings)
 with power_control() as p:
     for j,this_dB in enumerate(dB_settings):
         if j == 0:
-            print("starting log")
             p.start_log()
-        print("setting power")
         p.set_power(this_dB)
         for k in range(10):
             short_delay
-        print("sleeping again")
-        short_delay
-        for B0_index,desired_B0 in enumerate(r_[3503:3508:0.1]):
-            short_delay
+            if p.get_power_setting() >= this_dB: break
+        long_delay
+        meter_powers[j] = p.get_power_setting()
+        if True:
+            for B0_index,desired_B0 in enumerate(r_[3503:3508:0.1]):
+                short_delay
+                if True:
+                    long_delay
     log_array, log_dict = p.stop_log()# where error occurred originally!
     #}}}
 
