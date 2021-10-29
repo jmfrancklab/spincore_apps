dbc408f3f30e7ee28c3ac07cb2f210d811ae10b9
Alex Guinness (lab) <aguinnes@syr.edu>
2021-10-29 11:43:00 -0400

    field sweep ppg

--- SpinCore_pp/run_field_dep_justMw.py
+++ SpinCore_pp/run_field_dep_justMw.py
@@ -48,7 +48,7 @@ mw_freqs = []
 #stop_field = 33510
 #field_axis = start_field + (stop_field-start_field)*uneven
 
-field_axis = r_[3502:3508:.5]
+field_axis = r_[3422:3426:.5]
 print("Here is my field axis:",field_axis)
 
 # Parameters for Bridge12
@@ -63,10 +63,10 @@ input("Look ok?")
 powers = 1e-3*10**(dB_settings/10.)
 #}}}
 
-output_name = '150uM_TEMPO_toluene_cap_probe_field_dep'
+output_name = 'TEMPOL_heat_exch_289uM_field_dep'
 node_name = 'field_sweep_1'
-adcOffset = 25
-gamma_eff = (14.892825/3505.43)
+adcOffset = 28
+gamma_eff = (14.549013/3424.42)
 #{{{ acq params
 tx_phases = r_[0.0,90.0,180.0,270.0]
 amplitude = 1.0
@@ -84,7 +84,7 @@ if not phase_cycling:
 # as this is generally what the SpinCore takes
 # note that acq_time is always milliseconds
 #}}}
-p90 = 4.69
+p90 = 1.781
 deadtime = 10.0
 repetition = 12e6
 
@@ -94,7 +94,7 @@ nPoints = 1024*2
 acq_time = nPoints/SW_kHz # ms
 tau_adjust = 0
 deblank = 1.0
-tau = 1000.
+tau = 3500.
 pad = 0
 #{{{ setting acq_params dictionary
 acq_params = {}
@@ -118,125 +118,125 @@ if phase_cycling:
 #}}}
 
 with xepr() as x_server:
-    for B0_index,desired_B0 in enumerate(field_axis):
-        true_B0 = x_server.set_field(desired_B0)
-        print("My field in G is %f"%true_B0)
-        time.sleep(3.0)
-        carrierFreq_MHz = gamma_eff*true_B0
-        print("My frequency in MHz is",carrierFreq_MHz)
-        acq_params['carrierFreq_MHz'] = carrierFreq_MHz
-        data_length = 2*nPoints*nEchoes*nPhaseSteps
-        with Bridge12() as b:
-            b.set_wg(True)
-            b.set_rf(True)
-            b.set_amp(True)
-            this_return = b.lock_on_dip(ini_range=(9.819e9,9.825e9))
-            dip_f = this_return[2]
-            print("Frequency",dip_f)
-            mw_freqs.append(dip_f)
-            acq_params['mw_freqs'] = mw_freqs
-            b.set_freq(dip_f)
-            meter_powers = zeros_like(dB_settings)
-            for j,this_power in enumerate(r_[dB_settings]):
-                print("\n*** *** *** *** ***\n") 
-                if j>0 and this_power > last_power + 3:
+    with Bridge12() as b:
+        b.set_wg(True)
+        b.set_rf(True)
+        b.set_amp(True)
+        this_return = b.lock_on_dip(ini_range=(9.819e9,9.825e9))
+        dip_f = this_return[2]
+        print("Frequency",dip_f)
+        mw_freqs.append(dip_f)
+        acq_params['mw_freqs'] = mw_freqs
+        b.set_freq(dip_f)
+        meter_powers = zeros_like(dB_settings)
+        for j,this_power in enumerate(r_[dB_settings]):
+            print("\n*** *** *** *** ***\n") 
+            if j>0 and this_power > last_power + 3:
+                last_power += 3
+                print("SETTING TO...",last_power)
+                b.set_power(last_power)
+                time.sleep(3.0)
+                while this_power > last_power+3:
                     last_power += 3
                     print("SETTING TO...",last_power)
                     b.set_power(last_power)
                     time.sleep(3.0)
-                    while this_power > last_power+3:
-                        last_power += 3
-                        print("SETTING TO...",last_power)
-                        b.set_power(last_power)
+                print("FINALLY - SETTING TO DESIRED POWER")
+                b.set_power(this_power)
+            elif j == 0:
+                threshold_power = 10
+                if this_power > threshold_power:
+                    next_power = threshold_power + 3
+                    while next_power < this_power:
+                        print("SETTING To...",next_power)
+                        b.set_power(next_power)
                         time.sleep(3.0)
-                    print("FINALLY - SETTING TO DESIRED POWER")
-                    b.set_power(this_power)
-                elif j == 0:
-                    threshold_power = 10
-                    if this_power > threshold_power:
-                        next_power = threshold_power + 3
-                        while next_power < this_power:
-                            print("SETTING To...",next_power)
-                            b.set_power(next_power)
-                            time.sleep(3.0)
-                            next_power += 3
-                    b.set_power(this_power)
-                else:
-                    b.set_power(this_power)
-                time.sleep(15)
-                with prologix_connection() as p:
-                    with gigatronics(prologix_instance=p, address=7) as g:
-                        meter_powers = g.read_power()
-                        print("POWER READING",meter_powers)
-                for x in range(nScans):
-                    print("\n*** *** *** *** ***\n")
-                    print("\n*** *** ***\n")
-                    print("CONFIGURING TRANSMITTER...")
-                    SpinCore_pp.configureTX(adcOffset, carrierFreq_MHz, tx_phases, amplitude, nPoints)
-                    print("\nTRANSMITTER CONFIGURED.")
-                    print("***")
-                    print("CONFIGURING RECEIVER...")
-                    acq_time = SpinCore_pp.configureRX(SW_kHz, nPoints, nScans, nEchoes, nPhaseSteps) #ms
-                    print("\nRECEIVER CONFIGURED.")
-                    print("***")
-                    print("\nINITIALIZING PROG BOARD...\n")
-                    SpinCore_pp.init_ppg();
-                    print("\nLOADING PULSE PROG...\n")
-                    if phase_cycling:
-                        SpinCore_pp.load([
-                            ('marker','start',1),
-                            ('phase_reset',1),
-                            ('delay_TTL',deblank),
-                            ('pulse_TTL',p90,'ph1',r_[0,1,2,3]),
-                            ('delay',tau),
-                            ('delay_TTL',deblank),
-                            ('pulse_TTL',2.0*p90,0),
-                            ('delay',deadtime),
-                            ('acquire',acq_time),
-                            ('delay',repetition),
-                            ('jumpto','start')
-                            ])
-                        #{{{
-                    if not phase_cycling:
-                        SpinCore_pp.load([
-                            ('marker','start',nScans),
-                            ('phase_reset',1),
-                            ('delay_TTL',deblank),
-                            ('pulse_TTL',p90,0.0),
-                            ('delay',tau),
-                            ('delay_TTL',deblank),
-                            ('pulse_TTL',2.0*p90,0.0),
-                            ('delay',deadtime),
-                            ('acquire',acq_time),
-                            ('delay',repetition),
-                            ('jumpto','start')
-                            ])
-                        #}}}
-                    print("\nSTOPPING PROG BOARD...\n")
-                    SpinCore_pp.stop_ppg();
-                    print("\nRUNNING BOARD...\n")
-                    SpinCore_pp.runBoard();
-                    raw_data = SpinCore_pp.getData(data_length, nPoints, nEchoes, nPhaseSteps, output_name)
-                    raw_data.astype(float)
-                    data_array = []
-                    data_array[::] = complex128(raw_data[0::2]+1j*raw_data[1::2])
-                    print("COMPLEX DATA ARRAY LENGTH:",shape(data_array)[0])
-                    print("RAW DATA ARRAY LENGTH:",shape(raw_data)[0])
-                    dataPoints = float(np.shape(data_array)[0])
-                    if x == 0 and B0_index == 0:
-                        time_axis = np.linspace(0.0,nEchoes*nPhaseSteps*acq_time*1e-3,dataPoints)
-                        data = ndshape([len(data_array),nScans,len(field_axis),1],['t','nScans','Field','power']).alloc(dtype=np.complex128)
-                        data.setaxis('t',time_axis).set_units('t','s')
-                        data.setaxis('nScans',r_[0:nScans])
-                        data.setaxis('Field',field_axis)
-                        data.setaxis('power',r_[powers])
-                        data.name(node_name)
-                        data.set_prop('acq_params',acq_params)
-                    data['nScans',x]['Field',B0_index]['power',0] = data_array
-                last_power = this_power
-            SpinCore_pp.stopBoard();
-            print("FINISHED B0 INDEX %d..."%B0_index)
-            print("\n*** *** ***\n")
+                        next_power += 3
+                b.set_power(this_power)
+            else:
+                b.set_power(this_power)
+            time.sleep(15)
+            with prologix_connection() as p:
+                with gigatronics(prologix_instance=p, address=7) as g:
+                    meter_powers = g.read_power()
+                    print("POWER READING",meter_powers)
+        for B0_index,desired_B0 in enumerate(field_axis):
+            true_B0 = x_server.set_field(desired_B0)
+            print("My field in G is %f"%true_B0)
+            time.sleep(3.0)
+            carrierFreq_MHz = gamma_eff*true_B0
+            print("My frequency in MHz is",carrierFreq_MHz)
+            acq_params['carrierFreq_MHz'] = carrierFreq_MHz
+            data_length = 2*nPoints*nEchoes*nPhaseSteps
+            for x in range(nScans):
+                print("\n*** *** *** *** ***\n")
+                print("\n*** *** ***\n")
+                print("CONFIGURING TRANSMITTER...")
+                SpinCore_pp.configureTX(adcOffset, carrierFreq_MHz, tx_phases, amplitude, nPoints)
+                print("\nTRANSMITTER CONFIGURED.")
+                print("***")
+                print("CONFIGURING RECEIVER...")
+                acq_time = SpinCore_pp.configureRX(SW_kHz, nPoints, nScans, nEchoes, nPhaseSteps) #ms
+                print("\nRECEIVER CONFIGURED.")
+                print("***")
+                print("\nINITIALIZING PROG BOARD...\n")
+                SpinCore_pp.init_ppg();
+                print("\nLOADING PULSE PROG...\n")
+                if phase_cycling:
+                    SpinCore_pp.load([
+                        ('marker','start',1),
+                        ('phase_reset',1),
+                        ('delay_TTL',deblank),
+                        ('pulse_TTL',p90,'ph1',r_[0,1,2,3]),
+                        ('delay',tau),
+                        ('delay_TTL',deblank),
+                        ('pulse_TTL',2.0*p90,0),
+                        ('delay',deadtime),
+                        ('acquire',acq_time),
+                        ('delay',repetition),
+                        ('jumpto','start')
+                        ])
+                    #{{{
+                if not phase_cycling:
+                    SpinCore_pp.load([
+                        ('marker','start',nScans),
+                        ('phase_reset',1),
+                        ('delay_TTL',deblank),
+                        ('pulse_TTL',p90,0.0),
+                        ('delay',tau),
+                        ('delay_TTL',deblank),
+                        ('pulse_TTL',2.0*p90,0.0),
+                        ('delay',deadtime),
+                        ('acquire',acq_time),
+                        ('delay',repetition),
+                        ('jumpto','start')
+                        ])
+                    #}}}
+                print("\nSTOPPING PROG BOARD...\n")
+                SpinCore_pp.stop_ppg();
+                print("\nRUNNING BOARD...\n")
+                SpinCore_pp.runBoard();
+                raw_data = SpinCore_pp.getData(data_length, nPoints, nEchoes, nPhaseSteps, output_name)
+                raw_data.astype(float)
+                data_array = []
+                data_array[::] = complex128(raw_data[0::2]+1j*raw_data[1::2])
+                print("COMPLEX DATA ARRAY LENGTH:",shape(data_array)[0])
+                print("RAW DATA ARRAY LENGTH:",shape(raw_data)[0])
+                dataPoints = float(np.shape(data_array)[0])
+                if x == 0 and B0_index == 0:
+                    time_axis = np.linspace(0.0,nEchoes*nPhaseSteps*acq_time*1e-3,dataPoints)
+                    data = ndshape([len(data_array),nScans,len(field_axis),1],['t','nScans','Field','power']).alloc(dtype=np.complex128)
+                    data.setaxis('t',time_axis).set_units('t','s')
+                    data.setaxis('nScans',r_[0:nScans])
+                    data.setaxis('Field',field_axis)
+                    data.setaxis('power',r_[powers])
+                    data.name(node_name)
+                    data.set_prop('acq_params',acq_params)
+                data['nScans',x]['Field',B0_index]['power',0] = data_array
+                print("FINISHED B0 INDEX %d..."%B0_index)
+                print("\n*** *** ***\n")
+        last_power = this_power
+        SpinCore_pp.stopBoard();
 print("EXITING...")
 print("\n*** *** ***\n")
 save_file = True
