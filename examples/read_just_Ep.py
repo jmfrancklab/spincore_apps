1278b5769ce52b4c6c249a41c0f4b0fe6521496e
Alex <guinneak198@github.com>
2021-11-05 15:46:45 -0400

    works?!

--- examples/read_just_Ep.py
+++ examples/read_just_Ep.py
@@ -52,7 +52,7 @@ for filename,nodename,file_location in [
     opt_shift,sigma, my_mask = correl_align(s*mysgn,indirect_dim='time',
             signal_pathway=signal_pathway)
     s.ift('t2')
-    s *= np.exp(-1j*2*pi*opt_shift*s.fromaxis('t2'))
+    s *= np.exp(1j*2*pi*opt_shift*s.fromaxis('t2'))
     s.ft('t2')
     s.ift('t2')
     s.ft(['ph1'])
@@ -87,19 +87,18 @@ for filename,nodename,file_location in [
     #}}}
     #{{{Normalize and flip
     s_int /= np.real(s_int['time',0].data.item())
-    s_int['time',1:] *= -1
+    #s_int['time',1:] *= -1
     ini_time = s_int.C.getaxis('time')[1] - (s_int.C.getaxis('time')[-1]-s_int.C.getaxis('time')[-2])
     time_axis = s_int.getaxis('time')
     time_axis[0] = ini_time
     s_int.setaxis('time',time_axis)
-    print(s_int['time',:])
-    print(ini_time)
     for j in range(len(s_int.getaxis('time'))):
         time_axis[j] -= ini_time
     #}}}
     fl.next('E(p)')
     fl.plot(s_int['time',:-3],'ko',capsize=2,alpha=0.3)
     fl.plot(s_int['time',-3:],'ro',capsize=2,alpha=0.3)
+    fl.show();quit()
 #{{{create nddata of power vs time from log
 with h5py.File("211102_500uM_TEMPOL_test_final.h5",'r') as f:
     log_grp = f['log']
@@ -113,7 +112,7 @@ fig, (ax_Rx,ax_power) = plt.subplots(2,1, figsize=(10,8))
 fl.next("log figure",fig=fig)
 ax_Rx.set_ylabel('Rx/mV')
 start_time = read_array['time'][0]
-relative_time = read_array['time'] - start_time
+relative_time = read_array['time'] - ini_time
 ax_Rx.plot(relative_time, read_array['Rx'],'.')
 ax_power.set_ylabel('power/dBm')
 ax_power.plot(relative_time,read_array['power'],'.')
@@ -138,20 +137,29 @@ fl.plot(power_axis,'.')
 power_axis = nddata(read_array['power'],[-1],['time'])
 power_axis.setaxis('time',relative_time)
 power_axis.name('power')
-fl.next('power axis')
+fl.next('power axis W')
+coupler_atten = 22
+power_axis.data = (10**((power_axis.data+coupler_atten)/10+3))/1e6 #convert to W
 fl.plot(power_axis,'.')
 #}}}
 #{{{finding average power over steps
-dnp_time_axis = list(s_int.getaxis('time'))
-power_list = []
-for time_start,time_stop in zip(dnp_time_axis[:-1],dnp_time_axis[1:]):
-    power_list.append(power_axis['time':(time_start,time_stop)].mean('time'))
+dnp_time_axis1 = s_int.getaxis('time').copy()
+dnp_time_axis = nddata(s_int.getaxis('time').copy(),[-1],['time'])
+slop=2.0
+new_time_axis = dnp_time_axis.C.data - slop
+new_time_axis = nddata(new_time_axis,[-1],['time'])
+power_vs_time = ndshape(dnp_time_axis).alloc().set_units('time','s').set_error(0)
+power_vs_time.setaxis('time',new_time_axis.data)
+for j,(time_start,time_stop) in enumerate(zip(dnp_time_axis1[:-1],dnp_time_axis1[1:])):
+    power_vs_time['time',j] = power_axis['time':((time_start+slop),(time_stop-slop))].mean('time',std=True)
     plt.axvline(x=time_start)
-for j in range(len(power_list)):
-    s_int.getaxis('time')[j] = power_list[j].data
-s_int.rename('time','power (dBm)')
-fl.next('final E(p)')
-fl.plot(s_int['power (dBm)',:-3],'ko',capsize=2,alpha=0.3)
-fl.plot(s_int['power (dBm)',-3:],'ro',capsize=2,alpha=0.3)
-fl.show()
+power_vs_time.set_units('time','s')
+fl.plot(power_vs_time,'ro',human_units=False)    
+
+s_int.setaxis('time',power_vs_time.data)
+s_int.rename('time','power')
+fl.next('Final E(p)')
+fl.plot(s_int['power',:-3],'ko',capsize=2,alpha=0.3)
+fl.plot(s_int['power',-3:],'ro',capsize=2,alpha=0.3)
+fl.show();quit()
 
