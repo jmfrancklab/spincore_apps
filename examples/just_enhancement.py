91af00d5cd75eaaa14cb908ef284b0771c798811
John Franck (laptop) <jmfranck@syr.edu>
2021-11-16 11:22:31 -0500

    reviewed code together

--- examples/just_enhancement.py
+++ examples/just_enhancement.py
@@ -41,7 +41,6 @@ pul_prog = 'ODNP_v3'
 max_power = 3 #W
 power_steps = 18
 dB_settings = gen_powerlist(max_power,power_steps)
-power_start_times = []
 threedown = True
 if threedown:
     append_dB = [dB_settings[abs(10**(dB_settings/10.-3)-max_power*frac).argmin()]
@@ -51,11 +50,9 @@ print("dB_settings",dB_settings)
 print("correspond to powers in Watts",10**(dB_settings/10.-3))
 myinput = input("Look ok?")
 #}}}
-time_list = [time.time()]
 if myinput.lower().startswith('n'):
     raise ValueError("you said no!!!")
 powers = 1e-3*10**(dB_settings/10.)
-time_list.append(time.time())
 
 #{{{ enhancement curve pulse prog
 def run_scans(nScans, power_idx, DNP_data=None):
@@ -63,9 +60,12 @@ def run_scans(nScans, power_idx, DNP_data=None):
     that the first time this is run, it will be run with DNP_data=None and that
     after that, you will pass in DNP_data
     
-    assume that the power axis is 1 longer than the "powers" array
+    assume that the power axis is 1 longer than the "powers" array,
+    so that we can also store the thermally polarized signal in this array
     (note that powers and other parameters are defined globally w/in the
     script, as this function is not designed to be moved outside the module
+
+    this generates an "indirect" axis
     """
     print("about to run run_scans for",power_idx)
     ph1_cyc = r_[0,1,2,3]
@@ -119,23 +119,20 @@ def run_scans(nScans, power_idx, DNP_data=None):
         run_scans_time_list.append(time.time())
         run_scans_names.append('run')
         print("\nRUNNING BOARD...\n")
-        this_time_var = time.time()
         SpinCore_pp.runBoard()
-        power_start_times.append(this_time_var)
         run_scans_time_list.append(time.time())
         run_scans_names.append('get data')
         raw_data = SpinCore_pp.getData(data_length, nPoints, nEchoes, nPhaseSteps, output_name)
-        raw_data.astype(float)
         run_scans_time_list.append(time.time())
         run_scans_names.append('shape data')
-        data_array=[]
-        data_array[::] = complex128(raw_data[0::2]+1j*raw_data[1::2])
+        data_array = complex128(raw_data[0::2]+1j*raw_data[1::2])
         print("COMPLEX DATA ARRAY LENGTH:",shape(data_array)[0])
         print("RAW DATA ARRAY LENGTH:",shape(raw_data)[0])
-        dataPoints = float(shape(data_array)[0])
+        dataPoints = int(shape(data_array)[0])
         if DNP_data is None:
             time_axis = r_[0:dataPoints]/(SW_kHz*1e3) 
-            DNP_data = ndshape([len(powers)+1,nScans,len(time_axis)],['indirect','nScans','t']).alloc(dtype=complex128)
+            DNP_data = ndshape([len(powers)+1,nScans,dataPoints],['indirect','nScans','t']).alloc(dtype=complex128)
+            DNP_data.setaxis('indirect',zeros(len(powers)+1)).set_units('s')
             DNP_data.setaxis('t',time_axis).set_units('t','s')
             DNP_data.setaxis('nScans',r_[0:nScans])
             DNP_data.name('enhancement_curve')
@@ -146,51 +143,40 @@ def run_scans(nScans, power_idx, DNP_data=None):
         print("checkpoints:",this_array-this_array[0])
         print("time for each chunk",['%s %0.1f'%(run_scans_names[j],v) for j,v in enumerate(diff(this_array))])
         print("stored scan",x,"for power_idx",power_idx)
-        if nScans > 1:
-            DNP_data.setaxis('nScans',r_[0:nScans])
         return DNP_data
 #}}}
 ini_time = time.time() # needed b/c data object doesn't exist yet
 DNP_data = run_scans(nScans,0)
-time_list.append(time.time())
+time_axis_coords = DNP_data.getaxis('indirect')
+time_axis_coords[0] = ini_time
+DNP_data.set_prop('start_time', ini_time)
+DNP_data.set_prop('thermal_done_time', time.time())
 power_settings = zeros_like(dB_settings)
-time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
 with power_control() as p:
-    retval = p.dip_lock(9.81,9.83)
-    print(retval)
     for j, this_dB in enumerate(dB_settings):
         print("SETTING THIS POWER",this_dB,"(",dB_settings[j-1],powers[j],"W)")
         if j == 0:
+            retval = p.dip_lock(9.81,9.83)
+            print(retval)
             p.start_log()
-        time.sleep(0.5)    
         p.set_power(this_dB)
+        for k in range(10):
+            time.sleep(0.5)
+            if p.get_power_setting() >= this_dB: break
+        if p.get_power_setting() < this_dB: raise ValueError("After 10 tries, the power has still not settled")    
         time.sleep(5)
         power_settings[j] = p.get_power_setting()
+        time_axis_coords[j] = time.time()
         run_scans(nScans,j+1,DNP_data)
-        if j == dB_settings[-1]:
-            this_log=p.stop_log()
+    this_log=p.stop_log()
+DNP_data.set_prop('stop_time', time.time())
 acq_params = {j:eval(j) for j in dir() if j in ['adcOffset', 'carrierFreq_MHz', 'amplitude',
     'nScans', 'nEchoes', 'p90_us', 'deadtime_us', 'repetition_us', 'SW_kHz',
     'nPoints', 'tau_adjust_us', 'deblank_us', 'tau_us', 'nPhaseSteps', 'MWfreq', 'power_settings','pul_prog']}
 DNP_data.set_prop('acq_params',acq_params)
-# can delete this after
-print("*** *** *** *** *** *** ***")
-print("*** *** *** *** *** *** ***")
-print("*** *** *** *** *** *** ***")
-print("THIS IS MY POWER START TIMES",power_start_times)
-print("*** *** *** *** *** *** ***")
-print("*** *** *** *** *** *** ***")
-print("*** *** *** *** *** *** ***")
-for q in range(len(power_start_times)):
-    power_start_times[q] = power_start_times[q] - power_start_times[0]
-DNP_data.setaxis('indirect',power_start_times)
-log_array = this_log.total_log
-log_dict = this_log.log_dict
 myfilename = date+'_'+output_name+'.h5'
 DNP_data.chunk('t',['ph1','t2'],[4,-1])
 DNP_data.setaxis('ph1',r_[0,1,2,3]/4)
-time_list.append(time.time())
-time_array = array(time_list)
 while save_file:
     try:
         print("SAVING FILE...")
@@ -206,10 +192,3 @@ while save_file:
 with h5py.File(myfilename, 'a') as f:
     log_grp = f.create_group('log')
     hdf_save_dict_to_group(log_grp,this_log.__getstate__())
-    dset = log_grp.create_dataset("log",data=log_array)
-    dset.attrs['dict_len'] = len(log_dict)
-    for j,(k,v) in enumerate(log_dict.items()):
-        dset.attrs['key%d'%j] = k
-        dset.attrs['val%d'%j] = v
-        
-
