de89c3d35eefed59c270265a75e1913fb5de934c
Alex <guinneak198@github.com>
2022-01-27 17:05:22 -0500

    proper processing for new field sweep

--- examples/proc_fieldSweep.py
+++ examples/proc_fieldSweep.py
@@ -2,60 +2,81 @@ from pylab import *
 from pyspecdata import *
 from pyspecProcScripts import *
 from pyspecProcScripts import lookup_table
-#from scipy.optimize import leastsq,minimize,basinhopping
 from sympy import symbols
+import matplotlib.pyplot as plt
 fl = fl_mod()
 t2 = symbols('t2')
 filter_bandwidth = 20e3
-gamma_eff = (14.897706/3506.5)#(14.893851/3505.6) # MHz / G
-f_dip = 9.8216745#9.82103 # GHz
-for thisfile,exp_type,nodename,postproc,label_str,freq_slice,field_slice in [
-        ('211019_150uM_TEMPO_cap_probe_field_dep1','ODNP_NMR_comp/field_dependent',
-            'field_sweep_1','field_sweep_v1',
-            'TEMPO field sweep',(-1e3,1e3),(-400,250)),
+gamma_eff = (14.904100/3507.57) # MHz / G
+for thisfile,exp_type,nodename,postproc,label_str,freq_slice in [
+        ('220126_150mM_TEMPOL_field_dep','ODNP_NMR_comp/field_dependent',
+            'field_sweep','field_sweep_v1',
+            'TEMPO field sweep',(-250,250)),
         ]:
-    s = find_file(thisfile,exp_type=exp_type,expno=nodename)#,
-            #postproc=postproc,lookup=lookup_table)
-    nPoints = s.get_prop('acq_params')['nPoints']
-    nEchoes = s.get_prop('acq_params')['nEchoes']
-    SW_kHz = s.get_prop('acq_params')['SW_kHz']
-    nScans = s.get_prop('acq_params')['nScans']
-    s.reorder('t',first=True)
-    s.chunk('t',['ph1','t2'],[4,-1])
-    s.setaxis('ph1',r_[0.,1.,2.,3.]/4)
-    s.reorder('t2',first=False)
+    s = find_file(thisfile,exp_type=exp_type,expno=nodename,
+            postproc=postproc,lookup = lookup_table)
+    #{{{Obtain ESR frequency and chunk/reorder dimensions
+    v_B12 = (s.get_prop('acq_params')['mw_freqs'][0])/1e9
+    #}}}
+    #{{{DC offset correction
     t2_max = s.getaxis('t2')[-1]
     rx_offset_corr = s['t2':(t2_max*0.75,None)]
-    rx_offset_corr = rx_offset_corr.data.mean()
+    rx_offset_corr = rx_offset_corr.mean(['t2'])
     s -= rx_offset_corr
     s.ft(['ph1'])
     fl.next('raw data -- coherence channels')
-    s.reorder(['ph1','Field','power','t2'])
-    fl.image(s)
+    print(ndshape(s))
+    s.reorder(['ph1','indirect','power','t2'])
+    fl.image(s.C.setaxis(
+'indirect','#').set_units('indirect','scan #'))
+    #}}}
+    #{{{frequency filtering and rough center
     s.ft('t2',shift=True)
     fl.next('frequency domain raw data')
-    fl.image(s)
-    freqs = s.get_prop('acq_params')['mw_freqs']
-    s = s['t2':freq_slice]
+    fl.image(s.C.setaxis(
+'indirect','#').set_units('indirect','scan #'))
+    s = s['t2':(-1e3,1e3)]
     s=s['ph1',1]['power',0]['nScans',0].C
     s.ift('t2')
     s.ft('t2')
     s = s['t2':(-filter_bandwidth/2,filter_bandwidth/2)]
     s.ift('t2')
-    rough_center = abs(s).C.convolve('t2',0.0001).mean_all_but('t2').argmax('t2').item()
-    s.setaxis(t2-rough_center)
+    best_shift = hermitian_function_test(s)
+    s.setaxis('t2',lambda x: x-best_shift).register_axis({'t2':0})
     s.ft('t2')
     fl.next('line plots')
-    for z in range(len(s.getaxis('Field'))):
-        fl.plot(abs(s['Field',z]),label='%d'%z)
-    s_ = s['t2':field_slice].sum('t2')
+    v_NMR = []
+    offsets = []
+    s = s['indirect',:-1]
+    for z in range(len(s.getaxis('indirect')[:]['Field'])):
+        fl.plot(abs(s['indirect',z]),label = '%0.2f'%s.getaxis('indirect')[z]['Field'])
+        offset = abs(s['indirect',z].C).argmax('t2')
+        offsets.append(offset)
+        true_carrier_freq = s.getaxis('indirect')[z]['carrierFreq']
+        v_rf = true_carrier_freq*1e6 + (offset)
+        v_rf /= 1e6
+        plt.axvline(x=freq_slice[0])
+        plt.axvline(x=freq_slice[-1])
+        v_NMR.append(v_rf.data) 
+    s_ = s['t2':freq_slice].sum('t2')
+    #}}}
+    #{{{convert x axis to ppt = v_NMR/v_ESR
+    ppt = (v_NMR / v_B12)
+    s_.setaxis('indirect',ppt)
+    s_.rename('indirect','ppt')
+    #}}}
+    #{{{Fitting
     fl.next('sweep, without hermitian')
-    fl.plot(abs(s_),'o-')
+    fl.plot(abs(s_),'-o')
     field_idx = (abs(s_.data)).argmax()
-    fitting = abs(s_).polyfit('Field',order=2)
-    Field = nddata(r_[3502:3508:100j],'Field')
-    fl.plot(Field.eval_poly(fitting,'Field'),label='fit')
-    print("ESR frequency is %f"%(freqs[0]/1e9))
-    print('I found a max of the fit at',Field.eval_poly(fitting,'Field').argmax().item())
-    print('I found data max at', abs(s_).argmax().item())
+    fitting = abs(s_).polyfit('ppt',order=2)
+    x_min = s_.getaxis('ppt')[0]
+    x_max = s_.getaxis('ppt')[-1]
+    Field = nddata(r_[x_min:x_max:100j],'ppt')
+    fl.plot(Field.eval_poly(fitting,'ppt'),label='fit')
+    #}}}
+    logger.info(strm("ESR frequency is %f"%(v_B12)))
+    logger.info(strm('The fit finds a max with ppt value:',
+        Field.eval_poly(fitting,'ppt').argmax().item()))
+    logger.info(strm('The data finds a ppt value', abs(s_).argmax().item()))
 fl.show()
