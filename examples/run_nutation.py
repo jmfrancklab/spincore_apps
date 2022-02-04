f33ec5014ba45b148d0904a55ebb94ad913329b4
Alex <guinneak198@github.com>
2022-02-04 12:51:56 -0500

    nutation edits

--- examples/run_nutation.py
+++ examples/run_nutation.py
@@ -16,39 +16,62 @@ nScans = 1
 nEchoes = 1
 repetition = 0.5e6
 p90_range = linspace(1.,15.,10,endpoint=False)
+ph1_cyc = r_[0,2]
+ph2_cyc = r_[0,2]
+SW_kHz = 3.9 #24.0 originally
+acq_time = 1024.
+tau = 3500
 #}}}
 #{{{These should stay the same regardless of sample
 date = datetime.now().strftime('%y%m%d')
-ph1_cyc = r_[0,2]
-ph2_cyc = r_[0,2]
 # NOTE: Number of segments is nEchoes * nPhaseSteps
-SW_kHz = 3.9 #24.0 originally
-acq_time = 1024.
 nPoints = int(acq_time*SW_kHz+0.5)
 acq_time = nPoints/SW_kHz # ms
-tau = 3500
-print("ACQUISITION TIME:",acq_time,"ms")
-print("TAU DELAY:",tau,"us")
+logger.debug("ACQUISITION TIME:",acq_time,"ms")
+logger.debug("TAU DELAY:",tau,"us")
 #}}}
-nutation_data = run_spin_echo(nScans=nScans, indirect_idx = 0, 
-            indirect_len = len(p90_range), adcOffset = adcOffset,
-            carrierFreq_MHz = carrierFreq_MHz, nPoints = nPoints,
-            nEchoes=nEchoes, p90_us = p90_range[0], repetition = repetition,
-            tau_us = tau, SW_kHz = SW_kHz, output_name = output_name,
-            indirect_fields = None, ph1_cyc = ph1_cyc, ph2_cyc = ph2_cyc,
-            ret_data = None)
-p90_coords = nutation_data.getaxis('indirect')
-p90_coords[0]['p90_time'] = p90_range[0]
-for index,val in enumerate(p90_range[1:]):
-    p90 = val # us
-    run_spin_echo(nScans=nScans, indirect_idx = index+1, 
-            indirect_len = len(p90_range), adcOffset = adcOffset,
-            carrierFreq_MHz = carrierFreq_MHz, nPoints = nPoints,
-            nEchoes=nEchoes, p90_us = p90, repetition = repetition,
-            tau_us = tau, SW_kHz = SW_kHz, output_name = output_name,
-            indirect_fields = None, ph1_cyc = ph1_cyc, ph2_cyc = ph2_cyc,
+# {{{ check for file
+myfilename = date + "_" + output_name + ".h5"
+if os.path.exists(myfilename):
+    raise ValueError(
+        "the file %s already exists, so I'm not going to let you proceed!" % myfilename
+    )
+# }}}
+
+nutation_data = run_spin_echo(
+        nScans=nScans, 
+        indirect_idx = 0, 
+        indirect_len = len(p90_range), 
+        adcOffset = adcOffset,
+        carrierFreq_MHz = carrierFreq_MHz, 
+        nPoints = nPoints,
+        nEchoes=nEchoes, 
+        p90_us = p90_range[0], 
+        repetition = repetition,
+        tau_us = tau, 
+        SW_kHz = SW_kHz, 
+        output_name = output_name,
+        indirect_fields = None, 
+        ph1_cyc = ph1_cyc, 
+        ph2_cyc = ph2_cyc,
+        ret_data = None)
+for index,p90 in enumerate(p90_range[1:]):
+    run_spin_echo(
+            nScans=nScans, 
+            indirect_idx = index+1, 
+            indirect_len = len(p90_range), 
+            adcOffset = adcOffset,
+            carrierFreq_MHz = carrierFreq_MHz, 
+            nPoints = nPoints,
+            nEchoes=nEchoes, 
+            p90_us = p90, 
+            repetition = repetition,
+            tau_us = tau, 
+            SW_kHz = SW_kHz, 
+            output_name = output_name,
+            ph1_cyc = ph1_cyc, 
+            ph2_cyc = ph2_cyc,
             ret_data = nutation_data)
-    p90_coords[index+1]['p90_time'] = p90
 acq_params = {j:eval(j) for j in dir() if j in ['adcOffset', 'carrierFreq_MHz', 'amplitude',
     'nScans', 'nEchoes', 'p90_range', 'deadtime', 'repetition', 'SW_kHz',
     'nPoints', 'deblank_us', 'tau', 'nPhaseSteps']}
@@ -60,31 +83,10 @@ nutation_data.chunk('t',
         ['ph2','ph1','t2'],[len(ph1_cyc),len(ph2_cyc),-1]).setaxis(
                 'ph2',ph2_cyc/4).setaxis('ph1',ph1_cyc/4)
 nutation_data.reorder('t2',first=False)
-save_file=True
-while save_file:
-    try:
-        print("SAVING FILE...")
-        nutation_data.hdf5_write(myfilename,
-                directory=getDATADIR(exp_type='ODNP_NMR_comp/nutation'))
-        print("Name of saved data",nutation_data.name())
-        print("Units of saved data",nutation_data.get_units('t'))
-        print("Shape of saved data",ndshape(nutation_data))
-        save_file = False
-    except Exception as e:
-        print("\nEXCEPTION ERROR.")
-        print("FILE MAY ALREADY EXIST IN TARGET DIRECTORY.")
-        print("WILL TRY CURRENT DIRECTORY LOCATION...")
-        output_name = input("ENTER NEW NAME FOR FILE (AT LEAST TWO CHARACTERS):")
-        if len(output_name) is not 0:
-            nutation_data.hdf5_write(date+'_'+output_name+'.h5')
-            print("\n*** FILE SAVED WITH NEW NAME IN CURRENT DIRECTORY ***\n")
-            break
-        else:
-            print("\n*** *** ***")
-            print("UNACCEPTABLE NAME. EXITING WITHOUT SAVING DATA.")
-            print("*** *** ***\n")
-            break
-        save_file = False
+nutation_data.hdf5_write(myfilename,
+        directory=getDATADIR(exp_type='ODNP_NMR_comp/nutation'))
+logger.info(strm("Name of saved data",nutation_data.name()))
+logger.info(strm("Shape of saved data",ndshape(nutation_data)))
 SpinCore_pp.stopBoard()
 fl.next('raw data')
 fl.image(nutation_data)
