94bbd3b48b5e23a5d2b5c1a212801d62c9100135
John M. Franck (SU laptop) <jmfranck@syr.edu>
2022-07-12 14:39:45 -0400

    ran black on config parser function

--- examples/config_parser_fn.py
+++ examples/config_parser_fn.py
@@ -1,60 +1,63 @@
 from pyspecdata import *
 import configparser
+
+
 def parser_function(parser_filename):
-    #open parser
+    # open parser
     config = configparser.ConfigParser()
     config.sections()
-    config.read('active.ini')
-    acq_params = config['acq_params']
-    file_names = config['file_names']
-    odnp_params = config['odnp_params']
+    config.read("active.ini")
+    acq_params = config["acq_params"]
+    file_names = config["file_names"]
+    odnp_params = config["odnp_params"]
     retval = {}
     # {{{ floats, by section
-    for thisname in ['p90_us',
-            'deadtime_us',
-            'amplitude',
-            'deblank_us',
-            'tau_us',
-            'repetition_us',
-            'SW_kHz',
-            'acq_time_ms',
-            'carrierFreq_MHz',
-            ]:
+    for thisname in [
+        "p90_us",
+        "deadtime_us",
+        "amplitude",
+        "deblank_us",
+        "tau_us",
+        "repetition_us",
+        "SW_kHz",
+        "acq_time_ms",
+        "carrierFreq_MHz",
+    ]:
         retval[thisname] = float(acq_params[thisname.lower()])
     for thisname in [
-            'Field',
-            'uw_dip_center_GHz',
-            'uw_dip_width_GHz',
-            'FIR_rep',
-            ]:
+        "Field",
+        "uw_dip_center_GHz",
+        "uw_dip_width_GHz",
+        "FIR_rep",
+    ]:
         retval[thisname] = float(odnp_params[thisname.lower()])
     # }}}
     # {{{ int, by section
     for thisname in [
-            'nScans',
-            'adc_offset',
-            'nEchoes',
-            ]:
+        "nScans",
+        "adc_offset",
+        "nEchoes",
+    ]:
         retval[thisname] = int(acq_params[thisname.lower()])
     for thisname in [
-            'max_power',
-            'power_steps',
-            'num_T1s',
-            ]:
+        "max_power",
+        "power_steps",
+        "num_T1s",
+    ]:
         retval[thisname] = int(odnp_params[thisname.lower()])
     for thisname in [
-            'odnp_counter',
-            'echo_counter',
-            'IR_counter',
-            ]:
+        "odnp_counter",
+        "echo_counter",
+        "IR_counter",
+    ]:
         retval[thisname] = int(odnp_params[thisname.lower()])
     # }}}
     # {{{ all the rest of the file name parameters are just strings
     for thisname in [
-            'date',
-            'chemical',
-            'type',
-            ]:
+        "date",
+        "chemical",
+        "type",
+    ]:
         retval[thisname] = int(file_names[thisname.lower()])
     # }}}
-    return retval, config #return dictionary and also the config file itself 
+    return retval, config  # return dictionary and also the config file itself
