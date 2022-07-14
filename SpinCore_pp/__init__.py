9d6d5d25f21b8a597f9b8d090fba727eb67fc2b3
Alex <guinneak198@github.com>
2022-07-14 15:10:40 -0400

    comment out stuff so AG laptop runs

--- SpinCore_pp/__init__.py
+++ SpinCore_pp/__init__.py
@@ -1,32 +1,32 @@
-from .SpinCore_pp import (
-    pause,
-    configureRX,
-    configureTX,
-    init_ppg,
-    stop_ppg,
-    ppg_element,
-    runBoard,
-    load,
-    getData,
-    stopBoard,
-)
-from .SpinCore_pp import tune
+#from .SpinCore_pp import (
+#    pause,
+#    configureRX,
+#    configureTX,
+#    init_ppg,
+#    stop_ppg,
+#    ppg_element,
+#    runBoard,
+#    load,
+#    getData,
+#    stopBoard,
+#)
+#from .SpinCore_pp import tune
 from .config_parser_function import parser_function
-from .process_first_arg import process_args
+#from .process_first_arg import process_args
 
 __all__ = [
-    "SpinCore_pp",
-    "configureRX",
-    "configureTX",
-    "getData",
-    "init_ppg",
-    "load",
-    "pause",
-    "ppg_element",
-    "process_args",
-    "runBoard",
-    "stopBoard",
-    "stop_ppg",
+#    "SpinCore_pp",
+#    "configureRX",
+#    "configureTX",
+#    "getData",
+#    "init_ppg",
+#    "load",
+#    "pause",
+#    "ppg_element",
+#    "process_args",
+#    "runBoard",
+#    "stopBoard",
+#    "stop_ppg",
     "parser_function",
-    "tune",
+    #    "tune",
 ]
--- setup.py
+++ setup.py
@@ -9,25 +9,25 @@ ext_modules = []
 # {{{ compile_SpinCore_pp implies that these are set in bashrc -- I don't know
 #     how they get set in the first place
 arg_list = []
-if "conda_headers" not in os.environ.keys():
-    raise ValueError("I don't see the environment variables that I expect -- please edit environment.sh so it describes your system, and then run it w/ `source environment.sh`")
-for j in ["conda_headers","spincore","numpy"]:
-    arg_list.append('-I'+os.environ[j])
-for j in ["conda_libs","spincore"]:
-    arg_list.append('-L'+os.environ[j])
+##if "conda_headers" not in os.environ.keys():
+#    raise ValueError("I don't see the environment variables that I expect -- please edit environment.sh so it describes your system, and then run it w/ `source environment.sh`")
+#for j in ["conda_headers","spincore","numpy"]:
+#    arg_list.append('-I'+os.environ[j])
+#for j in ["conda_libs","spincore"]:
+#    arg_list.append('-L'+os.environ[j])
 # }}}
-ext_modules.append(Extension(name='SpinCore_pp._SpinCore_pp',
-    sources = ["SpinCore_pp/SpinCore_pp.i", "SpinCore_pp/SpinCore_pp.c"],
-    define_macros = [('ADD_UNDERSCORE',None)],
-    # flags from compile_SpinCore_pp.sh
-    extra_compile_args = [
-        "-shared",
-        "-DMS_WIN64",
-        ] + arg_list,# debug flags
-    library_dirs = ['.'],
-    libraries = ['mrispinapi64'],
-    swig_opts=['-threads'],
-    ))
+#ext_modules.append(Extension(name='SpinCore_pp._SpinCore_pp',
+#    sources = ["SpinCore_pp/SpinCore_pp.i", "SpinCore_pp/SpinCore_pp.c"],
+#    define_macros = [('ADD_UNDERSCORE',None)],
+#    # flags from compile_SpinCore_pp.sh
+#    extra_compile_args = [
+#        "-shared",
+#        "-DMS_WIN64",
+#        ] + arg_list,# debug flags
+#    library_dirs = ['.'],
+#    libraries = ['mrispinapi64'],
+#    swig_opts=['-threads'],
+#    ))
 exec(compile(open('SpinCore_pp/version.py', "rb").read(), 'SpinCore_pp/version.py', 'exec'))
 
 setup(
@@ -45,5 +45,5 @@ setup(
         "h5py",
         "pyserial>=3.0",
         ],
-    ext_modules = ext_modules,
+#    ext_modules = ext_modules,
 )
