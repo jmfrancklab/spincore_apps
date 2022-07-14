d932a7f754474e4bedb3f80cf467dd71fab82c16
John Franck (laptop) <jmfranck@syr.edu>
2022-07-14 13:15:16 -0400

    going to make the parser module accessible everywhere -- about to run sed

--- SpinCore_pp/__init__.py
+++ SpinCore_pp/__init__.py
@@ -1,6 +1,32 @@
-from .SpinCore_pp import pause, configureRX, configureTX, init_ppg, stop_ppg, ppg_element, runBoard, load, getData, stopBoard 
+from .SpinCore_pp import (
+    pause,
+    configureRX,
+    configureTX,
+    init_ppg,
+    stop_ppg,
+    ppg_element,
+    runBoard,
+    load,
+    getData,
+    stopBoard,
+)
 from .SpinCore_pp import tune
+from .config_parser_function import parser_function
 from .process_first_arg import process_args
-__all__ = ['SpinCore_pp','tune',
-        "pause", "configureRX", "configureTX", "init_ppg", "stop_ppg",
-        "ppg_element", "runBoard", "load", "process_args", "getData", "stopBoard"]
+
+__all__ = [
+    "SpinCore_pp",
+    "configureRX",
+    "configureTX",
+    "getData",
+    "init_ppg",
+    "load",
+    "pause",
+    "ppg_element",
+    "process_args",
+    "runBoard",
+    "stopBoard",
+    "stop_ppg",
+    "parser_function",
+    "tune",
+]
