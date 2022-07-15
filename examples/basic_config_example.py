4608a65d1af94de75a83b4f250db11f5f5d5817e
John M. Franck (SU laptop) <jmfranck@syr.edu>
2022-07-15 13:15:53 -0400

    ran black on basic config example

--- examples/basic_config_example.py
+++ examples/basic_config_example.py
@@ -3,9 +3,9 @@
 import configparser
 import os
 import SpinCore_pp
-retval_dict, config = SpinCore_pp.parser_function("active.ini")
-retval_dict['new_thing'] = 300
-print(retval_dict)
-retval_dict, config = SpinCore_pp.parser_function("active.ini")
-print(retval_dict)
 
+retval_dict, config = SpinCore_pp.parser_function("active.ini")
+retval_dict["new_thing"] = 300
+print(retval_dict)
+retval_dict, config = SpinCore_pp.parser_function("active.ini")
+print(retval_dict)
