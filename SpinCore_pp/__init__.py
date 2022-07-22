ba6c9f3329c9e3ca033bbbdb8cb745bdb7da72ec
Alex Guinness (lab) <aguinnes@syr.edu>
2022-07-22 10:16:04 -0400

    for nmr comp uncomment

--- SpinCore_pp/__init__.py
+++ SpinCore_pp/__init__.py
@@ -1,35 +1,35 @@
-#from .SpinCore_pp import (
-#    pause,
-#    configureRX,
-#    configureTX,
-#    init_ppg,
-#    stop_ppg,
-#    ppg_element,
-#    runBoard,
-#    load,
-#    getData,
-#    stopBoard,
-#)
-#from .SpinCore_pp import tune
+from .SpinCore_pp import (
+    pause,
+    configureRX,
+    configureTX,
+    init_ppg,
+    stop_ppg,
+    ppg_element,
+    runBoard,
+    load,
+    getData,
+    stopBoard,
+)
+from .SpinCore_pp import tune
 from .config_parser_fn import configuration
 from .calc_vdlist import vdlist_from_relaxivities, return_vdlist
-#from .process_first_arg import process_args
+from .process_first_arg import process_args
 
 __all__ = [
-#    "SpinCore_pp",
-#    "configureRX",
-#    "configureTX",
-#    "getData",
-#    "init_ppg",
-#    "load",
-#    "pause",
-#    "ppg_element",
-#    "process_args",
-#    "runBoard",
-#    "stopBoard",
-#    "stop_ppg",
+    "SpinCore_pp",
+    "configureRX",
+    "configureTX",
+    "getData",
+    "init_ppg",
+    "load",
+    "pause",
+    "ppg_element",
+    "process_args",
+    "runBoard",
+    "stopBoard",
+    "stop_ppg",
     "configuration",
     "return_vdlist",
     "vdlist_from_relaxivities",
-    #    "tune",
+    "tune",
 ]
