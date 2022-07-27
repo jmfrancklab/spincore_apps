124cadd869298fb03db06a40041572a0ce6cf1c7
Alex Guinness (lab) <aguinnes@syr.edu>
2022-07-27 16:11:22 -0400

    finding gamma works

--- examples/find_gamma_eff.py
+++ examples/find_gamma_eff.py
@@ -6,12 +6,9 @@ import SpinCore_pp
 config_dict = SpinCore_pp.configuration('active.ini')
 print("Your original gamma effective was:", config_dict['gamma_eff_MHz_G'])
 Delta_nu = float(sys.argv[1])/1e6
-new_gamma = config_dict['gamma_eff_MHz_G'] - (
-        Delta_nu/config_dict['Field'])
-print(type(new_gamma))
-print(type(Delta_nu))
+new_gamma = config_dict['gamma_eff_MHz_G'] * (1 - (Delta_nu/config_dict['carrierFreq_MHz'])) 
 print("based on your offset of %d"%Delta_nu)
-print("your new gamma_eff should be %d"%new_gamma)
+print("your new gamma_eff should be %.8f"%new_gamma)
 config_dict['gamma_eff_MHz_G'] = new_gamma
 config_dict.write()
 
