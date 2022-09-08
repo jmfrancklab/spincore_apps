3d6f8462bdba84f20963b6ef9a4b0740bbdccb8a
John M. Franck (SU laptop) <jmfranck@syr.edu>
2022-09-08 15:22:31 -0400

    just fix typo

--- examples/adc_offset.py
+++ examples/adc_offset.py
@@ -1,5 +1,5 @@
 import SpinCore_pp
 myconfig = SpinCore_pp.configuration("active.ini")
 myconfig['adc_offset'] = SpinCore_pp.adc_offset()
-print("You're ADC offset is:",myconfig['adc_offset'])
+print("Your ADC offset is:",myconfig['adc_offset'])
 myconfig.write()
