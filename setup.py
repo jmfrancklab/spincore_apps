0a6c1d4bd744f7af0ac5015ac6aafde2928b8225
John M. Franck (SU laptop) <jmfranck@syr.edu>
2022-10-24 11:23:11 -0400

    disable the API so some examples can be run on a computer w/out spincore

--- setup.py
+++ setup.py
@@ -9,31 +9,31 @@ ext_modules = []
 # {{{ compile_SpinCore_pp implies that these are set in bashrc -- I don't know
 #     how they get set in the first place
 arg_list = []
-if "conda_headers" not in os.environ.keys():
-    raise ValueError("I don't see the environment variables that I expect -- please edit environment.sh so it describes your system, and then run it w/ `source environment.sh`")
-for j in ["conda_headers","spincore","numpy"]:
-    arg_list.append('-I'+os.environ[j])
-for j in ["conda_libs","spincore"]:
-    arg_list.append('-L'+os.environ[j])
+#if "conda_headers" not in os.environ.keys():
+#    raise ValueError("I don't see the environment variables that I expect -- please edit environment.sh so it describes your system, and then run it w/ `source environment.sh`")
+#for j in ["conda_headers","spincore","numpy"]:
+#    arg_list.append('-I'+os.environ[j])
+#for j in ["conda_libs","spincore"]:
+#    arg_list.append('-L'+os.environ[j])
 # }}}
-ext_modules.append(Extension(name='SpinCore_pp._SpinCore_pp',
-    sources = ["SpinCore_pp/SpinCore_pp.i", "SpinCore_pp/SpinCore_pp.c"],
-    define_macros = [('ADD_UNDERSCORE',None)],
-    #  flags from compile_SpinCore_pp.sh
-    extra_compile_args = [
-        "-shared",
-        "-DMS_WIN64",
-        ] + arg_list,# debug flags
-    library_dirs = ['.'],
-    libraries = ['mrispinapi64'],
-    swig_opts=['-threads'],
-    ))
-exec(compile(open('SpinCore_pp/version.py', "rb").read(), 'SpinCore_pp/version.py', 'exec'))
-
+#ext_modules.append(Extension(name='SpinCore_pp._SpinCore_pp',
+#    sources = ["SpinCore_pp/SpinCore_pp.i", "SpinCore_pp/SpinCore_pp.c"],
+#    define_macros = [('ADD_UNDERSCORE',None)],
+#    #  flags from compile_SpinCore_pp.sh
+#    extra_compile_args = [
+#        "-shared",
+#        "-DMS_WIN64",
+#        ] + arg_list,# debug flags
+#    library_dirs = ['.'],
+#    libraries = ['mrispinapi64'],
+#    swig_opts=['-threads'],
+#    ))
+#exec(compile(open('SpinCore_pp/version.py', "rb").read(), 'SpinCore_pp/version.py', 'exec'))
+#
 setup(
     name='SpinCore_pp',
     author='Beaton,Franck,Guinness',
-    version=__version__,
+    version='0.1',
     packages=setuptools.find_packages(),
     license='LICENSE.md',
     author_email='jmfranck@notgiven.com',
