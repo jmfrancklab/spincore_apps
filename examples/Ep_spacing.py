1934f196401f057fc545867e516ccf5ffdea0acd
Alex <guinneak198@github.com>
2023-04-13 16:43:28 -0400

    dBm step config file paramter

--- examples/Ep_spacing.py
+++ examples/Ep_spacing.py
@@ -9,6 +9,7 @@ with figlist_var() as fl:
         est_phalf=config_dict["guessed_phalf"] / 4,
         max_power=config_dict["max_power"],
         p_steps=config_dict["power_steps"],
+        min_dBm_step=config_dict["min_dBm_step"],
         fl=fl,
     )
     plt.title("Evenly spaced Ep curve")
