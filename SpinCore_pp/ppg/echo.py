41402614f1040513d24c3c657b1ebfadf702c88c
Alex Guinness <aguinnes@syr.edu>
2024-02-13 14:18:25 -0500

    extra print not wanted

--- SpinCore_pp/ppg/echo.py
+++ SpinCore_pp/ppg/echo.py
@@ -93,7 +93,6 @@ def run_spin_echo(
     # times
     prog_p90_us = prog_plen(p90_us)
     prog_p180_us = prog_plen(2 * p90_us)
-    print(prog_p90_us)
     tx_phases = r_[0.0, 90.0, 180.0, 270.0]
     nPhaseSteps = len(ph1_cyc) * len(ph2_cyc)
     data_length = 2 * nPoints * nEchoes * nPhaseSteps
