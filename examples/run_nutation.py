5d7a9d43106ea30623bc5833dde06409b8916455
John Franck <jmfranck@jmfDebian.syr.edu>
2024-06-13 19:28:11 -0400

    try to fix various issues with the nutation example (missing amplitude setting and doubled-up call to ppg function

--- examples/run_nutation.py
+++ examples/run_nutation.py
@@ -25,7 +25,9 @@ date = datetime.now().strftime("%y%m%d")
 config_dict["type"] = "nutation"
 config_dict["date"] = date
 config_dict["echo_counter"] += 1
-filename = f"{config_dict['date']}_{config_dict['chemical']}_{config_dict['type']}"
+filename = (
+    f"{config_dict['date']}_{config_dict['chemical']}_{config_dict['type']}"
+)
 # }}}
 # {{{set phase cycling
 phase_cycling = True
@@ -44,80 +46,68 @@ thisB0 = xepr().set_field(B0)
 # }}}
 # {{{check total points
 total_pts = nPoints * nPhaseSteps
-assert total_pts < 2 ** 14, (
+assert total_pts < 2**14, (
     "You are trying to acquire %d points (too many points) -- either change SW or acq time so nPoints x nPhaseSteps is less than 16384\nyou could try reducing the acq_time_ms to %f"
     % (total_pts, config_dict["acq_time_ms"] * 16384 / total_pts)
 )
 # }}}
-nutation_data = run_spin_echo(
-    nScans = config_dict["nScans"],
-    indirect_idx = 0,
-    indirect_len = len(p90_range_us),
-    ph1_cyc = ph1_cyc,
-    adcOffset = config_dict["adc_offset"],
-    carrierFreq_MHz = config_dict["carrierFreq_MHz"],
-    nPoints = nPoints,
-    nEchoes = config_dict["nEchoes"],
-    p90_us = p90_range_us[0],
-    repetition_us = config_dict["repetition_us"],
-    tau_us = config_dict["tau_us"],
-    SW_kHz = config_dict["SW_kHz"],
-    indirect_fields = ("p_90", "index"),
-    ret_data = None,
-)
-mytimes = nutation_data.getaxis("indirect")
-mytimes[0]["p_90"] = p90_range_us[0]
-for index, val in enumerate(p90_range_us[1:]):
-    p90_us = val  # us
-    mytimes[index + 1]["p_90"] = p90_us
+nutation_data = None
+for idx, p90_us in enumerate(p90_range_us):
     run_spin_echo(
-        nScans = config_dict["nScans"],
-        indirect_idx = index + 1,
-        indirect_len = len(p90_range_us),
-        ph1_cyc = ph1_cyc,
-        adcOffset = config_dict["adc_offset"],
-        carrierFreq_MHz = config_dict["carrierFreq_MHz"],
-        nPoints = nPoints,
-        nEchoes = config_dict["nEchoes"],
-        p90_us = p90_us,
-        repetition_us = config_dict["repetition_us"],
-        tau_us = config_dict["tau_us"],
-        SW_kHz = config_dict["SW_kHz"],
-        indirect_fields = ("p_90", "index"),
-        ret_data = nutation_data,
+        nScans=config_dict["nScans"],
+        indirect_idx=idx,
+        indirect_len=len(p90_range_us),
+        ph1_cyc=ph1_cyc,
+        amplitude=config_dict["amplitude"],
+        adcOffset=config_dict["adc_offset"],
+        carrierFreq_MHz=config_dict["carrierFreq_MHz"],
+        nPoints=nPoints,
+        nEchoes=config_dict["nEchoes"],
+        p90_us=p90_us,
+        repetition_us=config_dict["repetition_us"],
+        tau_us=config_dict["tau_us"],
+        SW_kHz=config_dict["SW_kHz"],
+        indirect_fields=("p_90", "index"),
+        ret_data=nutation_data,
     )
-    mytimes[j+1]["p_90"] = p90_val
+# {{{ for some reason, axis coordinates were
+#     given as a structured array with p90
+#     and index fields, so I'm staying
+#     consistent
+mytimes = nutation_data.getaxis("indirect")
+mytimes["p_90"][:] = p90_range_us
+mytimes["index"][:] = arange(len(p90_range_us))
+# }}}
 if phase_cycling:
     nutation_data.chunk("t", ["ph1", "t2"], [4, -1])
     nutation_data.setaxis("ph1", ph1_cyc)
     if config_dict["nScans"] > 1:
         nutation_data.setaxis("nScans", r_[0 : config_dict["nScans"]])
-    nutation_data.reorder(["ph1","nScans", "t2"])
+    nutation_data.reorder(["ph1", "nScans", "t2"])
     nutation_data.squeeze()
     nutation_data.set_units("t2", "s")
     fl.next("Raw - time")
-    fl.image(
-        nutation_data.C.mean("nScans"))
+    fl.image(nutation_data.C.mean("nScans"))
     nutation_data.reorder("t2", first=False)
     for_plot = nutation_data.C
-    for_plot.ft('t2',shift=True)
-    for_plot.ft(['ph1'], unitary = True)
-    fl.next('FTed data')
-    fl.image(for_plot.C.mean("nScans")
-    )
+    for_plot.ft("t2", shift=True)
+    for_plot.ft(["ph1"], unitary=True)
+    fl.next("FTed data")
+    fl.image(for_plot.C.mean("nScans"))
 else:
     if config_dict["nScans"] > 1:
         nutation_data.setaxis("nScans", r_[0 : config_dict["nScans"]])
     nutation_data.rename("t", "t2")
     fl.next("Raw - time")
-    fl.image(
-        nutation_data.C.mean("nScans"))
+    fl.image(nutation_data.C.mean("nScans"))
     nutation_data.reorder("t2", first=False)
     for_plot = nutation_data.C
-    for_plot.ft('t2',shift=True)
-    fl.next('FTed data')
+    for_plot.ft("t2", shift=True)
+    fl.next("FTed data")
     fl.image(for_plot)
-nutation_data.name(config_dict["type"] + "_" + str(config_dict["echo_counter"]))
+nutation_data.name(
+    config_dict["type"] + "_" + str(config_dict["echo_counter"])
+)
 nutation_data.set_prop("postproc_type", "spincore_nutation_v1")
 nutation_data.set_prop("acq_params", config_dict.asdict())
 target_directory = getDATADIR(exp_type="ODNP_NMR_comp/nutation")
@@ -129,7 +119,9 @@ if os.path.exists(f"{filename_out}"):
         os.path.normpath(os.path.join(target_directory, f"{filename_out}"))
     ) as fp:
         if nodename in fp.keys():
-            print("this nodename already exists, so I will call it temp_nutation")
+            print(
+                "this nodename already exists, so I will call it temp_nutation"
+            )
             nutation_data.name("temp_nutation")
             nodename = "temp_nutation"
         nutation_data.hdf5_write(f"{filename_out}", directory=target_directory)
