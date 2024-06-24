78ca5a9b365c38dad0bec9b7cdc91cce134d154a
John Franck <jmfranck@jmfDebian.syr.edu>
2024-06-24 16:33:34 -0400

    diff two ppgs

--- examples/run_generic_echo.py
+++ examples/run_generic_echo.py
@@ -8,15 +8,24 @@ import os
 from datetime import datetime
 import h5py
 from Instruments.XEPR_eth import xepr
+
+# {{{ command-line option to leave the field untouched (if you set it once, why set it again)
 import sys
 
 adjust_field = True
 if len(sys.argv) == 2 and sys.argv[1] == "stayput":
     adjust_field = False
+# }}}
 
 # {{{importing acquisition parameters
 config_dict = SpinCore_pp.configuration("active.ini")
-nPoints, config_dict['SW_kHz'], config_dict['acq_time_ms'] = get_integer_sampling_intervals(config_dict['SW_kHz'], config_dict['acq_time_ms'])
+(
+    nPoints,
+    config_dict["SW_kHz"],
+    config_dict["acq_time_ms"],
+) = get_integer_sampling_intervals(
+    config_dict["SW_kHz"], config_dict["acq_time_ms"]
+)
 my_exp_type = "ODNP_NMR_comp/Echoes"
 target_directory = getDATADIR(exp_type=my_exp_type)
 assert os.path.exists(target_directory)
@@ -26,9 +35,7 @@ date = datetime.now().strftime("%y%m%d")
 config_dict["type"] = "echo"
 config_dict["date"] = date
 config_dict["cpmg_counter"] += 1
-filename = (
-    f"{config_dict['date']}_{config_dict['chemical']}_generic_{config_dict['type']}"
-)
+filename = f"{config_dict['date']}_{config_dict['chemical']}_generic_{config_dict['type']}"
 # }}}
 # {{{let computer set field
 if adjust_field:
@@ -57,6 +64,7 @@ nPhaseSteps = len(ph2) * len(ph_diff)
 # }}}
 prog_p90_us = prog_plen(config_dict["p90_us"])
 prog_p180_us = prog_plen(2 * config_dict["p90_us"])
+# PR COMMENT:  missing assert statements present in cpmg
 # {{{check total points
 total_pts = nPoints * nPhaseSteps * config_dict["nEchoes"]
 assert total_pts < 2**14, (
@@ -69,10 +77,10 @@ data = generic(
         ("phase_reset", 1),
         ("delay_TTL", config_dict["deblank_us"]),
         ("pulse_TTL", prog_p90_us, "ph_cyc", ph1_cyc),
-        (
-            "delay",
-            config_dict["tau_us"]
-        ),
+        # PR COMMENT: tau_us isn't defined the same way here as in the
+        # CPMG sequence (it's not defined to mean the same thing), which
+        # seems a recipe for trouble
+        ("delay", config_dict["tau_us"]),
         ("delay_TTL", config_dict["deblank_us"]),
         ("pulse_TTL", prog_p180_us, "ph_cyc", ph2_cyc),
         ("delay", config_dict["deadtime_us"]),
@@ -91,14 +99,12 @@ data = generic(
 )
 ## {{{ chunk and save data
 data.set_prop("postproc_type", "spincore_diffph_SE_v1")
-data.set_prop("coherence_pathway", {"ph_overall":-1,"ph1":+1})
+data.set_prop("coherence_pathway", {"ph_overall": -1, "ph1": +1})
 data.set_prop("acq_params", config_dict.asdict())
-data.name(config_dict["type"] + "_" + str(config_dict["cpmg_counter"]))
-data.chunk(
-    "t",
-    ["ph2", "ph_diff", "t2"], 
-    [len(ph2), len(ph_diff), -1]
-)
+# PR COMMENT: def of nodename was objectively convoluted -- I fix
+nodename = config_dict["type"] + "_" + str(config_dict["cpmg_counter"])
+data.name(nodename)
+data.chunk("t", ["ph2", "ph_diff", "t2"], [len(ph2), len(ph_diff), -1])
 data.labels(
     {
         "ph2": r_[0 : len(ph2)],
@@ -109,14 +115,17 @@ data.setaxis("ph2", ph2 / 4)
 data.setaxis("ph_diff", ph_diff / 4)
 # }}}
 filename_out = filename + ".h5"
-nodename = data.name()
 if os.path.exists(f"{filename_out}"):
     print("this file already exists so we will add a node to it!")
     with h5py.File(
         os.path.normpath(os.path.join(target_directory, f"{filename_out}"))
     ) as fp:
         if nodename in fp.keys():
-            print("this nodename already exists, so I will call it temp_generic_echo")
+            print("this nodename already exists, so I will call it temp_cpmg")
+            # PR COMMENT: this is objectively not a good solution.  You
+            # should rather increment the counter and re-determine the
+            # node name.  I actually thought we may have a ppg that does
+            # this already.
             data.name("temp_generic_echo")
             nodename = "temp_generic_echo"
     data.hdf5_write(f"{filename_out}", directory=target_directory)
@@ -125,15 +134,20 @@ else:
         data.hdf5_write(f"{filename_out}", directory=target_directory)
     except:
         print(
-            f"I had problems writing to the correct file {filename}.h5, so I'm going to try to save your file to temp_generic_echo.h5 in the current h5 file"
+            f"I had problems writing to the correct file {filename}.h5, so I'm going to try to save your file to temp.h5 in the current h5 file"
         )
-        if os.path.exists("temp_generic_echo.h5"):
-            print("there is a temp_generic_echo.h5 already! -- I'm removing it")
-            os.remove("temp_generic_echo.h5")
-        data.hdf5_write("temp_generic_echo.h5")
+        if os.path.exists("temp.h5"):
+            print("there is a temp.h5 already! -- I'm removing it")
+            os.remove("temp.h5")
+        data.hdf5_write("temp.h5")
         print(
-            "if I got this far, that probably worked -- be sure to move/rename temp_generic_echo.h5 to the correct name!!"
+            "if I got this far, that probably worked -- be sure to move/rename temp.h5 to the correct name!!"
         )
 print("\n*** FILE SAVED IN TARGET DIRECTORY ***\n")
-print("saved data to (node, file, exp_type):", data.name(), filename_out, my_exp_type)
+print(
+    "saved data to (node, file, exp_type):",
+    data.name(),
+    filename_out,
+    my_exp_type,
+)
 config_dict.write()
