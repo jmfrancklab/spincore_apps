5d83f2ceb290aed6496f976553b65dbbd20cdba7
Alex Guinness <aguinnes@syr.edu>
2024-06-25 13:20:45 -0400

    keep plotting because it was in og

--- examples/run_Hahn_echo.py
+++ examples/run_Hahn_echo.py
@@ -58,44 +58,49 @@ with xepr() as x:
     print("field set to ", field_G)
 # }}}
 # {{{set phase cycling
+# default phase cycling of run_spin_echo is to use a 4 step on the 90 pulse
+# so this is here just for setting the chunked axis later and calculating the
+# total points
 ph1_cyc = r_[0, 1, 2, 3]
 nPhaseSteps = 4
 # }}}
 # {{{check total points
 total_pts = nPoints * nPhaseSteps
-assert total_pts < 2**14, (
+assert total_pts < 2 ** 14, (
     "You are trying to acquire %d points (too many points) -- either change SW or acq time so nPoints x nPhaseSteps is less than 16384\nyou could try reducing the acq_time_ms to %f"
     % (total_pts, config_dict["acq_time_ms"] * 16384 / total_pts)
 )
 # }}}
-data = run_spin_echo(
-    nScans=config_dict["nScans"],
-    indirect_idx=0,
-    indirect_len=1,
-    ph1_cyc=ph1_cyc,
-    adcOffset=config_dict["adc_offset"],
-    carrierFreq_MHz=config_dict["carrierFreq_MHz"],
-    nPoints=nPoints,
-    nEchoes=1,
-    p90_us=config_dict["p90_us"],
-    repetition_us=config_dict["repetition_us"],
-    tau_us=config_dict["tau_us"],
-    SW_kHz=config_dict["SW_kHz"],
-    ret_data=None,
+# {{{ acquire echo
+echo_data = run_spin_echo(
+    nScans = config_dict["nScans"],
+    indirect_idx = 0,
+    indirect_len = 1,
+    ph1_cyc = ph1_cyc,
+    adcOffset = config_dict["adc_offset"],
+    carrierFreq_MHz = config_dict["carrierFreq_MHz"],
+    nPoints = nPoints,
+    nEchoes = 1, #you should never be running a hahn echo with >1 echo
+    p90_us = config_dict["p90_us"],
+    repetition_us = config_dict["repetition_us"],
+    tau_us = config_dict["tau_us"],
+    SW_kHz = config_dict["SW_kHz"],
+    ret_data = None,
 )
-## {{{ chunk and save data
-data.set_prop("postproc_type", "spincore_SE_v1")
-data.set_prop("coherence_pathway", {"ph1": +1})
-data.set_prop("acq_params", config_dict.asdict())
+# }}}
+# {{{ chunk and save data
+echo_data.set_prop("postproc_type", "spincore_SE_v1")
+echo_data.set_prop("coherence_pathway", {"ph1": +1})
+echo_data.set_prop("acq_params", config_dict.asdict())
 nodename = config_dict["type"] + "_" + str(config_dict["echo_counter"])
-data.name(nodename)
-data.chunk(
+echo_data.name(nodename)
+echo_data.chunk(
     "t", 
     ["ph1", "t2"], 
     [len(ph1_cyc), -1],
 )
-data.setaxis("ph1", ph1_cyc / 4)
-data.reorder(["ph1", "nScans", "t2"])
+echo_data.setaxis("ph1", ph1_cyc / 4)
+echo_data.reorder(["ph1", "nScans", "t2"])
 filename_out = filename + ".h5"
 if os.path.exists(f"{filename_out}"):
     print("this file already exists so we will add a node to it!")
@@ -108,28 +113,28 @@ if os.path.exists(f"{filename_out}"):
             nodename = "%s_temp_%d" % (orig_nodename, tempcounter)
             data.name(nodename)
             tempcounter += 1
-data.hdf5_write(f"{filename_out}", directory=target_directory)
+echo_data.hdf5_write(f"{filename_out}", directory=target_directory)
 print("\n*** FILE SAVED IN TARGET DIRECTORY ***\n")
 print(
     "saved data to (node, file, exp_type):", 
-    data.name(), 
+    echo_data.name(), 
     filename_out, 
     my_exp_type,
 )
 config_dict.write()
 # }}}
-data.ft("t2", shift=True)
+echo_data.ft("t2", shift=True)
 with figlist_var() as fl:
     fl.next("image - ft")
-    fl.image(data)
+    fl.image(echo_data)
     fl.next("image - ft, coherence")
-    data.ft("ph1")
-    fl.image(data)
+    echo_data.ft("ph1")
+    fl.image(echo_data)
     fl.next("data plot")
-    if "nScans" in data.dimlabels:
-        data_slice = data["ph1", 1].mean("nScans")
+    if "nScans" in echo_data.dimlabels:
+        data_slice = echo_data["ph1", 1].mean("nScans")
     else:
-        data_slice = data["ph1", 1]
+        data_slice = echo_data["ph1", 1]
     fl.plot(data_slice, alpha=0.5)
     fl.plot(data_slice.imag, alpha=0.5)
     fl.plot(abs(data_slice), color="k", alpha=0.5)
