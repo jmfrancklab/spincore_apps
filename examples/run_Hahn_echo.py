eb210d93c72ec0e317ad231660d88fa0ea602158
Warren Kincaid <wfkincai@syr.edu>
2024-07-02 16:18:47 -0400

    WK add amplitude param to pull from ini

--- examples/run_Hahn_echo.py
+++ examples/run_Hahn_echo.py
@@ -11,18 +11,15 @@ the NMR computer to set the field etc.
 
 from pylab import *
 from pyspecdata import *
-import pyspecProcScripts
-import subprocess, os
+import os
 import SpinCore_pp
-from SpinCore_pp import get_integer_sampling_intervals
+from SpinCore_pp import get_integer_sampling_intervals, save_data
 from SpinCore_pp.ppg import run_spin_echo
 from datetime import datetime
 from Instruments.XEPR_eth import xepr
-import h5py
 
 my_exp_type = "ODNP_NMR_comp/Echoes"
-target_directory = getDATADIR(exp_type=my_exp_type)
-assert os.path.exists(target_directory)
+assert os.path.exists(getDATADIR(exp_type=my_exp_type))
 # {{{importing acquisition parameters
 config_dict = SpinCore_pp.configuration("active.ini")
 (
@@ -31,17 +28,13 @@ config_dict = SpinCore_pp.configuration("active.ini")
     config_dict["acq_time_ms"],
 ) = get_integer_sampling_intervals(
     SW_kHz=config_dict["SW_kHz"],
-    acq_time_ms=config_dict["acq_time_ms"],
+    time_per_segment_ms=config_dict["acq_time_ms"],
 )
 # }}}
-# {{{create filename and save to config file
-date = datetime.now().strftime("%y%m%d")
+# {{{add file saving parameters to config dict
 config_dict["type"] = "echo"
-config_dict["date"] = date
+config_dict["date"] = datetime.now().strftime("%y%m%d")
 config_dict["echo_counter"] += 1
-filename = (
-    f"{config_dict['date']}_{config_dict['chemical']}_{config_dict['type']}"
-)
 # }}}
 # {{{set phase cycling
 # default phase cycling of run_spin_echo is to use a 4 step on the 90 pulse
@@ -50,12 +43,10 @@ filename = (
 ph1_cyc = r_[0, 1, 2, 3]
 nPhaseSteps = 4
 # }}}
-print(
-    "I'm assuming that you've tuned your probe to",
-    config_dict["carrierFreq_MHz"],
-    "since that's what's in your .ini file."
+input(
+    "I'm assuming that you've tuned your probe to %f since that's what's in your .ini file. Hit enter if this is true"
+    % config_dict["carrierFreq_MHz"]
 )
-input("Hit enter if this is true")
 # {{{ let computer set field
 field_G = config_dict["carrierFreq_MHz"] / config_dict["gamma_eff_MHz_G"]
 print(
@@ -104,22 +95,7 @@ data.reorder(["ph1", "nScans", "t2"])
 data.set_prop("postproc_type", "spincore_SE_v1")
 data.set_prop("coherence_pathway", {"ph1": +1})
 data.set_prop("acq_params", config_dict.asdict())
-nodename = config_dict["type"] + "_" + str(config_dict["echo_counter"])
-data.name(nodename)
-filename_out = filename + ".h5"
-if os.path.exists(f"{filename_out}"):
-    print("this file already exists so we will add a node to it!")
-    with h5py.File(
-        os.path.normpath(os.path.join(target_directory, f"{filename_out}"))
-    ) as fp:
-        while nodename in fp.keys():
-            nodename = (
-                config_dict["type"] + "_" + str(config_dict["echo_counter"])
-            )
-            data.name(nodename)
-            config_dict["echo_counter"] += 1
-data.hdf5_write(f"{filename_out}", directory=target_directory)
-print("\n*** FILE SAVED IN TARGET DIRECTORY ***\n")
+config_dict = save_data(data, my_exp_type, config_dict, "echo")
 print(
     "Your *current* γ_eff (MHz/G) should be ",
     config_dict["gamma_eff_MHz_G"],
@@ -127,18 +103,6 @@ print(
     field_G,
     "), where Δν is your resonance offset",
 )
-print(
-    "saved data to (node, file, exp_type):",
-    data.name(),
-    filename_out,
-    my_exp_type,
-)
 config_dict.write()
 # }}}
-env = os.environ
-subprocess.call("which python",shell=True)
-subprocess.call((" ".join(["python",os.path.join(
-    os.path.split(os.path.split(pyspecProcScripts.__file__)[0])[0],
-    "examples", "proc_raw.py"),
-    data.name(),filename_out, my_exp_type])),
-    env=env)
+
