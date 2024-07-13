28e204b8ac768322243893feb7f411ff62164963
Alex Guinness <aguinnes@syr.edu>
2024-07-13 08:02:45 -0400

    feed configureRX one scan

--- SpinCore_pp/ppg/echo.py
+++ SpinCore_pp/ppg/echo.py
@@ -96,13 +96,14 @@ def run_spin_echo(
     tx_phases = r_[0.0, 90.0, 180.0, 270.0]
     nPhaseSteps = len(ph1_cyc) * len(ph2_cyc)
     data_length = 2 * nPoints * nEchoes * nPhaseSteps
+    for_RX_nScans = 1
     for nScans_idx in range(nScans):
         run_scans_time_list = [time.time()]
         run_scans_names = ["configure"]
         configureTX(adcOffset, carrierFreq_MHz, tx_phases, amplitude, nPoints)
         run_scans_time_list.append(time.time())
         run_scans_names.append("configure Rx")
-        acq_time_ms = configureRX(SW_kHz, nPoints, nScans, nEchoes, nPhaseSteps)
+        acq_time_ms = configureRX(SW_kHz, nPoints, for_RX_nScans, nEchoes, nPhaseSteps)
         run_scans_time_list.append(time.time())
         run_scans_names.append("init")
         init_ppg()
