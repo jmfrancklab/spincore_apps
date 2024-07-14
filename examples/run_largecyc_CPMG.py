5f6c5a7d89c3ee89617507788e387a02646538a2
Alex Guinness <aguinnes@syr.edu>
2024-07-14 16:54:49 -0400

    ran black and flake on largecyc_cpmg

--- examples/run_largecyc_CPMG.py
+++ examples/run_largecyc_CPMG.py
@@ -100,17 +100,12 @@ config_dict["tau_us"] = (
     2 * config_dict["deadtime_us"] + 1e3 * config_dict["echo_acq_ms"]
 ) / 2
 assert (
-    config_dict["tau_us"]
-    > 2 * prog_p90_us / pi + marker_us + config_dict["deblank_us"]
+    config_dict["tau_us"] > 2 * prog_p90_us / pi + marker_us + config_dict["deblank_us"]
 )
 assert config_dict["deadtime_us"] > config_dict["deblank_us"] + 2 * marker_us
 print(
     "If you are measuring on a scope, the time from the start (or end) of one 180 pulse to the next should be %0.1f us"
-    % (
-        2 * config_dict["deadtime_us"]
-        + 1e3 * config_dict["echo_acq_ms"]
-        + prog_p180_us
-    )
+    % (2 * config_dict["deadtime_us"] + 1e3 * config_dict["echo_acq_ms"] + prog_p180_us)
 )
 # }}}
 # {{{check total points
@@ -141,9 +136,7 @@ data = generic(
         ("acquire", config_dict["echo_acq_ms"]),
         (
             "delay",
-            config_dict["deadtime_us"]
-            - 2 * marker_us
-            - config_dict["deblank_us"],
+            config_dict["deadtime_us"] - 2 * marker_us - config_dict["deblank_us"],
         ),
         ("delay", marker_us),  # placeholder for jumpto
         # NOTE: here the tau_us is defined as
@@ -157,9 +150,7 @@ data = generic(
         ("acquire", config_dict["echo_acq_ms"]),
         (
             "delay",
-            config_dict["deadtime_us"]
-            - 2 * marker_us
-            - config_dict["deblank_us"],
+            config_dict["deadtime_us"] - 2 * marker_us - config_dict["deblank_us"],
         ),
         ("jumpto", "echo_label"),
         # In the line above I assume this takes
@@ -188,9 +179,9 @@ data.chunk(
     ["ph1", "ph2", "ph_overall", "nEcho", "t2"],
     [len(ph1), len(ph2), len(ph_overall), config_dict["nEchoes"], -1],
 )
-data.setaxis("nEcho", r_[0 : config_dict["nEchoes"]]).setaxis(
-    "ph1", ph1 / 4
-).setaxis("ph2", ph2 / 4).setaxis("ph_overall", ph_overall / 4)
+data.setaxis("nEcho", r_[0 : config_dict["nEchoes"]]).setaxis("ph1", ph1 / 4).setaxis(
+    "ph2", ph2 / 4
+).setaxis("ph_overall", ph_overall / 4)
 data.set_prop("postproc_type", "spincore_generalproc_v3")
 data.set_prop(
     "coherence_pathway",
